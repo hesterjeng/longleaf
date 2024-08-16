ce2bc38c34255702dc6891f15bb991d58217a43c
John Hester <john@imandra.ai>
2024-08-16 14:27:45 -0500

    feat: better python

--- src/longleaf.ml
+++ src/longleaf.ml
@@ -1,58 +1,37 @@
+module Log = (val Logs.src_log Logs.(Src.create "longleaf"))
+
 module A = struct
   let x = 0
 end
 
-(* Define a module with a type and some functions *)
-module MyMath = struct
-  type operation = Add | Subtract | Multiply | Divide
+module Libraries = struct
+  let numpy = ref Py.none
 
-  let apply_operation op x y =
-    match op with
-    | Add ->
-        x + y
-    | Subtract ->
-        x - y
-    | Multiply ->
-        x * y
-    | Divide ->
-        if y <> 0 then x / y else failwith "Division by zero"
+  let pandas = ref Py.none
 
-  let to_string op =
-    match op with
-    | Add ->
-        "+"
-    | Subtract ->
-        "-"
-    | Multiply ->
-        "*"
-    | Divide ->
-        "/"
+  let yfinance = ref Py.none
+
+  let init () =
+    try
+      numpy := Py.import "numpy" ;
+      pandas := Py.import "pandas" ;
+      yfinance := Py.import "yfinance"
+    with Py.E _ as e ->
+      Log.err (fun k ->
+          k "@[Failed to import module, are we in the correct environment?@]@." ) ;
+      raise e
 end
 
-(* Use the module *)
-let () =
-  let x = 10 in
-  let y = 5 in
-  let ops = [MyMath.Add; MyMath.Subtract; MyMath.Multiply; MyMath.Divide] in
-  List.iter
-    (fun op ->
-      let result = MyMath.apply_operation op x y in
-      let op_str = MyMath.to_string op in
-      Printf.printf "%d %s %d = %d\n" x op_str y result )
-    ops ;
-  (* More match statements outside the module *)
-  let check_sign num =
-    match num with
-    | n when n > 0 ->
-        "positive"
-    | n when n < 0 ->
-        "negative"
-    | _ ->
-        "zero"
-  in
-  let numbers = [x; -y; 0] in
-  List.iter
-    (fun n ->
-      let sign = check_sign n in
-      Printf.printf "The number %d is %s.\n" n sign )
-    numbers
+module Dataframe = struct
+  let index x =
+    Option.(
+      let+ index = Py.Object.get_attr_string x "index" in
+      index )
+end
+
+let run () =
+  Libraries.init () ;
+  let _ = Python_examples.ocaml_value_in_python () in
+  let _ = Python_examples.ocaml_function_in_python () in
+  let _ = Python_examples.call_python_function_from_ocaml () in
+  ()
