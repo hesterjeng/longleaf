8143edc0ba8e15c98013958888ac6667f74e0b42
John Hester <john@imandra.ai>
2024-08-19 18:03:58 -0500

    feat: data improvements, json serialization

--- src/data.ml
+++ src/data.ml
@@ -1,65 +1,37 @@
 module Log = (val Logs.src_log Logs.(Src.create "data"))
+open Util
 
 module Farray = struct
   (* type t = (float, Bigarray.float64_elt, Bigarray.c_layout) Bigarray.Genarray.t [@@deriving show] *)
   type t = Owl_dense_ndarray.D.arr
 
-  let pp fmt x = Owl_dense_ndarray.D.print ~fmt x
-  let of_pyobject x : t = Numpy.to_bigarray Bigarray.float64 Bigarray.c_layout x
+  let pp fmt x = Owl_dense_ndarray.Generic.pp_dsnda fmt x
+
+  let of_pyobject x : t =
+    let numpy_array = Py.Object.call_method x "to_numpy" [||] in
+    Util.pyprint numpy_array;
+    Numpy.to_bigarray Bigarray.float64 Bigarray.c_layout numpy_array
+
+  let yojson_of_t x =
+    let arr = Owl_dense_ndarray.D.to_array x in
+    yojson_of_array yojson_of_float arr
+
+  let t_of_yojson x =
+    let arr = array_of_yojson float_of_yojson x in
+    Owl_dense_ndarray.D.of_array arr [| Array.length arr |]
 end
 
 type t = {
   name : string;
-  date : Time.t array;
+  date : Farray.t;
   open_p : Farray.t;
   high_p : Farray.t;
   low_p : Farray.t;
   close_p : Farray.t;
   adj_close : Farray.t;
-  volume : int array;
+  volume : Farray.t;
 }
-[@@deriving show]
-(** Column oriented "dataframe".  We are maintaining an invariant that when we are working with
-the pandas dataframes, we are removing the index and making it a column. *)
-
-(* type data = t *)
-
-(** Alternative structure that may be a little bit easier to read/work with *)
-(* module Row_oriented = struct *)
-
-(*   type datapoint = *)
-(*     { name: string *)
-(*     ; date: Time.t *)
-(*     ; open_p: float *)
-(*     ; high_p: float *)
-(*     ; low_p: float *)
-(*     ; close_p: float *)
-(*     ; adj_close: float *)
-(*     ; volume: int } *)
-(*   [@@deriving show] *)
-
-(*   type t = datapoint array *)
-
-(*   let of_data (x : data) : t = *)
-(*     let length = Array.length x.date in *)
-(*     let init : datapoint option array = Array.make length None in *)
-(*     for i = 0 to length do *)
-(*       let datapoint = *)
-(*         Some *)
-(*           { name= x.name *)
-(*           ; date= x.date.(i) *)
-(*           ; open_p= Float.Array.(x.open_p.(i)) *)
-(*           ; high_p= x.high_p.(i) *)
-(*           ; low_p= x.low_p.(i) *)
-(*           ; close_p= x.close_p.(i) *)
-(*           ; adj_close= x.adj_close.(i) *)
-(*           ; volume= x.volume.(i) } *)
-(*       in *)
-(*       Array.set init i datapoint *)
-(*     done ; *)
-(*     let res = Array.filter_map Fun.id init in *)
-(*     res *)
-(* end *)
+[@@deriving show, yojson]
 
 (** Uses Python and the yfinance library to download information about the tickers.
  There may be multiple tickers here. *)
@@ -77,75 +49,43 @@ let get_data period tickers : Pytypes.pyobject =
   let dataframe_reset = Py.Callable.to_function reset_index [||] in
   dataframe_reset
 
-(* let of_pyobject name (dataframe : Pytypes.pyobject) : t = *)
-(*   let open Pyops in *)
-(*   let date = *)
-(*     dataframe.!$["Date"] *)
-(*     |> ( Py.List.to_array_map *)
-(*        @@ fun datetime64 -> *)
-(*        Py.Object.call_method datetime64 "timestamp" [||] |> Py.Float.to_float ) *)
-(*     |> Array.map Time.of_timestamp *)
-(*   in *)
-(*   let open_p = dataframe.!$["Open"] |> Py.List.to_array_map Py.Float.to_float in *)
-(*   let high_p = dataframe.!$["High"] |> Py.List.to_array_map Py.Float.to_float in *)
-(*   let low_p = dataframe.!$["Low"] |> Py.List.to_array_map Py.Float.to_float in *)
-(*   let close_p = *)
-(*     dataframe.!$["Close"] |> Py.List.to_array_map Py.Float.to_float *)
-(*   in *)
-(*   let adj_close = *)
-(*     dataframe.!$["Adj Close"] |> Py.List.to_array_map Py.Float.to_float *)
-(*   in *)
-(*   let volume = dataframe.!$["Volume"] |> Py.List.to_array_map Py.Int.to_int in *)
-(*   let length = Array.length date in *)
-(*   let convert_array =  *)
-(*   {name; date; open_p; high_p; low_p; close_p; adj_close; volume} *)
-
 let of_pyobject name (dataframe : Pytypes.pyobject) : t =
   let open Pyops in
   let date =
-    dataframe.!$["Date"]
-    |> ( Py.List.to_array_map @@ fun datetime64 ->
-         Py.Object.call_method datetime64 "timestamp" [||] |> Py.Float.to_float
-       )
-    |> Array.map Time.of_timestamp
+    (dataframe.!$["Date"]
+    |> Py.List.to_array_map @@ fun datetime64 ->
+       Py.Object.call_method datetime64 "timestamp" [||] |> Py.Float.to_float)
+    |> fun x -> Owl_dense_ndarray.D.of_array x [| Array.length x |]
   in
-  (* let open_p = dataframe.!$["Open"] |> Py.List.to_array_map Py.Float.to_float in *)
   let open_p = Farray.of_pyobject dataframe.!$["Open"] in
   let high_p = Farray.of_pyobject dataframe.!$["High"] in
   let low_p = Farray.of_pyobject dataframe.!$["Low"] in
   let close_p = Farray.of_pyobject dataframe.!$["Close"] in
   let adj_close = Farray.of_pyobject dataframe.!$["Adj Close"] in
-  let volume = dataframe.!$["Volume"] |> Py.List.to_array_map Py.Int.to_int in
-  let length = Array.length date in
-  (* let convert_array = *)
+  (* Log.err (fun k -> k "@[Trying volume@]@."); *)
+  let volume =
+    dataframe.!$["Volume"]
+    |> Py.List.to_array_map Py.Int.to_int
+    |> Array.map Float.of_int
+    |> fun x -> Owl_dense_ndarray.D.of_array x [| Array.length x |]
+  in
   { name; date; open_p; high_p; low_p; close_p; adj_close; volume }
 
-(* let to_pyobject (x : t) = *)
-(*   let pandas = Py.import "pandas" in *)
-(*   let data_dict = Py.Dict.create () in *)
-(*   Py.Dict.set_item_string data_dict "Open" @@ *)
-(*   Py.Array.numpy x.open_p *)
-
-(*   ; *)
+let to_pyobject (x : t) =
+  let open Pyops in
+  let pandas = Py.import "pandas" in
+  let data_dict = Py.Dict.create () in
+  Py.Dict.set_item_string data_dict "Date" @@ Numpy.of_bigarray x.open_p;
+  Py.Dict.set_item_string data_dict "Open" @@ Numpy.of_bigarray x.open_p;
+  Py.Dict.set_item_string data_dict "High" @@ Numpy.of_bigarray x.open_p;
+  Py.Dict.set_item_string data_dict "Low" @@ Numpy.of_bigarray x.open_p;
+  Py.Dict.set_item_string data_dict "Close" @@ Numpy.of_bigarray x.open_p;
+  Py.Dict.set_item_string data_dict "Adj Close" @@ Numpy.of_bigarray x.open_p;
+  Py.Dict.set_item_string data_dict "Volume" @@ Numpy.of_bigarray x.open_p;
+  let dataframe = pandas.&("DataFrame") [| data_dict |] in
+  dataframe
 
 (** Given a single ticker, use the yfinance API to download its data and put it into type `data`  *)
 let of_string (name : string) =
   let dataframe = get_data "1mo" [| name |] in
   of_pyobject name dataframe
-
-(** Use Python json parsing to read the file x into a Data.t *)
-let of_json (x : string) =
-  let open Pyops in
-  let pandas = Py.import "pandas" in
-  let read_json = pandas.&("read_json") in
-  let tables = read_json [| Py.String.of_string x |] in
-  let to_datetime = Py.Module.get_function_with_keywords pandas "to_datetime" in
-  tables.!$["Date"] <-
-    (let dt = tables.!$["Date"] in
-     to_datetime [| dt |] [ ("unit", Py.String.of_string "s") ]);
-  (* let set_index = tables.@$("set_index") |> Py.Callable.to_function in *)
-  (* let _ = set_index [|Py.String.of_string "Date"|] in *)
-  tables
-
-(* let to_json (x : t) = *)
-(*   let pandas = Py.import "pandas" in *)
