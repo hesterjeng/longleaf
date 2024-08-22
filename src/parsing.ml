f75f138ebf478fdb34a2cda8b24676934d13361a
John Hester <john@imandra.ai>
2024-08-22 12:23:28 -0500

    feat: add parsing.ml

---
+++ src/parsing.ml
@@ -0,0 +1,18 @@
+open Angstrom
+
+let parens p = char '(' *> p <* char ')'
+
+let quoted_string =
+  char '\'' *> take_while1 (fun c -> not @@ Char.equal c '\'') <* char '\''
+
+let parser =
+  parens
+  @@ lift2
+       (fun s1 s2 -> (s1, s2))
+       (quoted_string <* char ',' <* char ' ')
+       quoted_string
+
+let top (input : string) =
+  match parse_string ~consume:Consume.All parser input with
+  | Ok s -> s
+  | Error e -> invalid_arg e
