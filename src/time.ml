915a64dcd4abcf871e1d3371c5a54b416df2a786
John Hester <hesterj@etableau.com>
2024-09-09 18:12:33 -0500

    fix: time parsing

--- src/time.ml
+++ src/time.ml
@@ -1,19 +1,19 @@
-type t = { date : Ptime.date; time : Ptime.time }
+module Log = (val Logs.src_log Logs.(Src.create "time"))
 
-let pp fmt x =
-  let ptime = Ptime.of_date_time (x.date, x.time) in
-  match ptime with
-  | Some time -> Format.fprintf fmt "%a" Ptime.pp time
-  | None -> invalid_arg "Illegal datetime!"
+type t = Ptime.t
 
-let of_timestamp ts =
-  match Ptime.of_float_s ts with
-  | Some date_time ->
-      let date, time = Ptime.to_date_time date_time in
-      { date; time }
-  | None -> invalid_arg "invalid date/time"
+let pp = Ptime.pp
 
-let print_time_array x =
-  Owl_dense_ndarray.D.iter
-    (fun x -> of_timestamp x |> fun x -> Format.printf "@[%a@]@." pp x)
-    x
+let of_int ms =
+  let int = Int64.of_int ms in
+  let seconds = Int64.div int 1000L in
+  let frac_s = Int64.to_int (Int64.rem int 1000L) in
+  match
+    Ptime.of_float_s (Int64.to_float seconds +. (float_of_int frac_s /. 1000.0))
+  with
+  | Some ptime -> ptime
+  | None -> failwith "Invalid timestamp"
+
+let pp_int fmt x =
+  let x = of_int x in
+  pp fmt x
