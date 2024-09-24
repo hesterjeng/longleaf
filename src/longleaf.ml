97618f812c8b68041dae63baa7ed96ba1341315e
John Hester <hesterj@etableau.com>
2024-09-24 14:44:11 -0500

    chore: get it building

--- src/longleaf.ml
+++ src/longleaf.ml
@@ -15,24 +15,6 @@ module Tickers = struct
     Py.List.to_array_map Py.Object.to_string ticker_symbols
 end
 
-(* module Dataframe = struct *)
-(*   let index x = *)
-(*     Option.( *)
-(*       let+ index = Py.Object.get_attr_string x "index" in *)
-(*       index) *)
-(* end *)
-
-let run () =
-  let _ = Python_examples.ocaml_value_in_python () in
-  let _ = Python_examples.ocaml_function_in_python () in
-  let _ = Python_examples.call_python_function_from_ocaml () in
-  let data = Data.of_string "AAPL" in
-  Log.app (fun k -> k "%a" Data.pp data);
-  Yojson.Safe.to_file "mo.json" @@ Data.yojson_of_t data;
-  (* My_time.print_time_array data.date; *)
-  Strategy.basic data;
-  ()
-
 let process_json (x : Yojson.Safe.t) =
   Log.app (fun k -> k "Attempting to create dataframe from json");
   Dataframe.of_json x
