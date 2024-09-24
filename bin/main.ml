a67b7619f234652a3d3d0de9bc9f7140998892f3
John Hester <hesterj@etableau.com>
2024-09-24 14:54:59 -0500

    feat: add Main.Vars

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,17 @@
+module Vars = struct
+  type env_variables = {
+    apca_api_key_id : string;
+    apca_api_secret_key : string;
+    apca_api_base_url : string;
+  }
+
+  let make () =
+    let apca_api_key_id = Unix.getenv "APCA_API_KEY_ID" in
+    let apca_api_secret_key = Unix.getenv "APCA_API_SECRET_KEY" in
+    let apca_api_base_url = Unix.getenv "APCA_API_BASE_URL" in
+    { apca_api_key_id; apca_api_secret_key; apca_api_base_url }
+end
+
 let () =
   Fmt_tty.setup_std_outputs ();
   let reporter = Logs_fmt.reporter () in
