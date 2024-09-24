7c5ade28eb37e4963d1e51d9727777633e8e8d73
John Hester <hesterj@etableau.com>
2024-09-24 16:18:00 -0500

    feat: basic GET request

---
+++ src/api.ml
@@ -0,0 +1,19 @@
+open Lwt
+open Lwt.Syntax
+open Cohttp
+open Cohttp_lwt_unix
+module Log = (val Logs.src_log Logs.(Src.create "api"))
+
+let get_account (env : Environment.t) =
+  let uri = Uri.with_path env.apca_api_base_url "/v2/account" in
+  let headers =
+    Header.init () |> fun h ->
+    Header.add h "APCA-API-KEY-ID" env.apca_api_key_id |> fun h ->
+    Header.add h "APCA-API-SECRET-KEY" env.apca_api_secret_key
+  in
+  let* resp, body = Client.get ~headers uri in
+  let* body = Cohttp_lwt.Body.to_string body in
+  let body_json = Yojson.Safe.from_string body in
+  let status = Response.status resp |> Code.string_of_status in
+  Log.app (fun k -> k "@[%s@]@.@[%a@]@." status Yojson.Safe.pp body_json);
+  Lwt.return_unit
