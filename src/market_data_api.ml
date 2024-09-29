2034af09b22f778494643da9ca3236d970e0c1e8
John Hester <hesterj@etableau.com>
2024-09-29 17:54:13 -0500

    feat: historical bars/historical auctions requests

--- src/market_data_api.ml
+++ src/market_data_api.ml
@@ -8,12 +8,29 @@ open Trading_types
 let h = Trading_api.h
 
 module Stock = struct
-
-  let historical_autions
-      (env : Environment.t)
-      (symbols : string list)
-      (start : Ptime.t)
-      (end_ : Ptime.t) =
+  let historical_auctions (env : Environment.t) (symbols : string list) =
+    let headers = h env in
+    let symbols = String.concat "," symbols in
     let uri = Uri.with_path env.apca_api_base_url "/v2/stocks/auctions" in
+    let body =
+      Yojson.Safe.(`Assoc [ ("symbols", `String symbols) ])
+      |> Yojson.Safe.to_string |> Cohttp_lwt.Body.of_string
+    in
+    let* response, body_stream = Client.post ~headers ~body uri in
+    let* resp_body = Cohttp_lwt.Body.to_string body_stream in
+    let status = Response.status response |> Code.string_of_status in
+    Lwt.return (status, resp_body)
 
+  let historical_bars (env : Environment.t) (symbols : string list) =
+    let headers = h env in
+    let symbols = String.concat "," symbols in
+    let uri = Uri.with_path env.apca_api_base_url "/v2/stocks/bars" in
+    let body =
+      Yojson.Safe.(`Assoc [ ("symbols", `String symbols) ])
+      |> Yojson.Safe.to_string |> Cohttp_lwt.Body.of_string
+    in
+    let* response, body_stream = Client.post ~headers ~body uri in
+    let* resp_body = Cohttp_lwt.Body.to_string body_stream in
+    let status = Response.status response |> Code.string_of_status in
+    Lwt.return (status, resp_body)
 end
