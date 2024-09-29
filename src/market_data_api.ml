d92be6ba160f4a9d20697c3dc516f3575e426dd6
John Hester <hesterj@etableau.com>
2024-09-28 20:42:55 -0500

    wip: historical auctions

--- src/market_data_api.ml
+++ src/market_data_api.ml
@@ -6,3 +6,14 @@ module Log = (val Logs.src_log Logs.(Src.create "data-api"))
 open Trading_types
 
 let h = Trading_api.h
+
+module Stock = struct
+
+  let historical_autions
+      (env : Environment.t)
+      (symbols : string list)
+      (start : Ptime.t)
+      (end_ : Ptime.t) =
+    let uri = Uri.with_path env.apca_api_base_url "/v2/stocks/auctions" in
+
+end
