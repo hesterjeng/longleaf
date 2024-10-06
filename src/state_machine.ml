a2f92a03b0b57719d3b654868b253c0ab49580c7
John Hester <hesterj@etableau.com>
2024-10-06 02:35:13 -0500

    wip: backtesting state machines

--- src/state_machine.ml
+++ src/state_machine.ml
@@ -32,8 +32,10 @@ end
 module type BACKEND = sig
   module Ticker : TICKER
 
+  val backtesting : bool
+
   val create_order :
-    Environment.t -> Trading_types.Order.t -> float -> Yojson.Safe.t Lwt.t
+    Environment.t -> Trading_types.Order.t -> Yojson.Safe.t Lwt.t
 
   (* val get_account : Environment.t -> Trading_api.Accounts.t Lwt.t *)
   val latest_bars : Environment.t -> string list -> Trading_types.Bars.t Lwt.t
@@ -41,52 +43,65 @@ module type BACKEND = sig
   (* val get_clock : Environment.t -> Trading_api.Clock.t Lwt.t *)
 end
 
-module Backtesting_backend : BACKEND = struct
-  open Lwt.Syntax
+module type BACKEND_INPUT = sig
+  val bars : Trading_types.Bars.t
+end
+
+(* Backtesting *)
+module Backtesting_backend (Data : BACKEND_INPUT) : BACKEND = struct
   open Trading_types
   module Ticker = InstantTicker
 
+  let backtesting = true
   let position : (string, int) Hashtbl.t = Hashtbl.create 0
   let cash = ref 100000.0
 
-  let create_order _ (x : Order.t) (price : float) : Yojson.Safe.t Lwt.t =
+  let create_order _ (x : Order.t) : Yojson.Safe.t Lwt.t =
     let symbol = x.symbol in
     let current_amt = Hashtbl.get position symbol |> Option.get_or ~default:0 in
     let qty = x.qty in
-    match x.side with
-    | Buy ->
+    match (x.side, x.order_type) with
+    | Buy, Market ->
         Hashtbl.replace position symbol (current_amt + qty);
-        cash := !cash -. (price *. Float.of_int qty);
+        cash := !cash -. (x.price *. Float.of_int qty);
         Lwt.return `Null
-    | Sell ->
+    | Sell, Market ->
         Hashtbl.replace position symbol (current_amt - qty);
-        cash := !cash +. (price *. Float.of_int qty);
+        cash := !cash +. (x.price *. Float.of_int qty);
         Lwt.return `Null
+    | _, _ -> invalid_arg "Backtesting can't handle this yet."
 
-  (* TODO *)
+  (* TODO: Massage historical bars to give them in a format we can use *)
+  (* for price data *)
   let latest_bars _ _ = invalid_arg "NYI"
 end
 
+(* Live trading *)
 module Alpaca_backend : BACKEND = struct
   open Lwt.Syntax
   open Trading_types
   module Ticker = ThirtyMinuteTicker
 
+  module Backtesting_backend = Backtesting_backend (struct
+    let bars = Bars.empty
+  end)
+
+  let backtesting = false
   let get_account = Trading_api.Accounts.get_account
   let latest_bars = Market_data_api.Stock.latest_bars
   let get_clock = Trading_api.Clock.get
 
-  let latest_price env ticker =
-    let* response = latest_bars env [ ticker ] in
-    let bars = response.bars in
-    match List.Assoc.get ~eq:String.equal ticker bars with
-    | Some [ info ] -> Lwt.return info.closing_price
-    | Some _ -> invalid_arg "Multiple bar items on latest bar?"
-    | None -> invalid_arg "Unable to get price info for ticker"
+  (* let latest_price env ticker = *)
+  (*   let* response = latest_bars env [ ticker ] in *)
+  (*   let bars = response.bars in *)
+  (*   match List.Assoc.get ~eq:String.equal ticker bars with *)
+  (*   | Some [ info ] -> Lwt.return info.closing_price *)
+  (*   | Some _ -> invalid_arg "Multiple bar items on latest bar?" *)
+  (*   | None -> invalid_arg "Unable to get price info for ticker" *)
 
-  let create_order env order price =
+  let create_order env order =
     let* res = Trading_api.Orders.create_market_order env order in
-    let* _ = Backtesting_backend.create_order env order price in
+    let* _ = Backtesting_backend.create_order env order in
     Lwt.return res
 end
 
@@ -94,57 +109,55 @@ module type STRAT = sig
   val run : Environment.t -> Cohttp.Code.status_code Lwt.t
 end
 
-module Make (Backend : BACKEND) = struct
-  module SimpleStateMachine : STRAT = struct
-    let () = Random.self_init ()
+module SimpleStateMachine (Backend : BACKEND) : STRAT = struct
+  let () = Random.self_init ()
 
-    open Trading_types
-    open State
-    open Lwt.Syntax
-    module Log = (val Logs.src_log Logs.(Src.create "simple-state-machine"))
+  open Trading_types
+  open State
+  open Lwt.Syntax
+  module Log = (val Logs.src_log Logs.(Src.create "simple-state-machine"))
 
-    let step (state : 'a State.t) : ('a, 'b) State.status Lwt.t =
-      let env = state.env in
-      match state.current with
-      | Initialize ->
-          Log.app (fun k -> k "Initialize");
-          Lwt.return @@ continue { state with current = Listening }
-      | Listening ->
-          let* () = Backend.Ticker.tick () in
-          Lwt.return @@ continue { state with current = Ordering }
-      | Ordering ->
-          Log.app (fun k -> k "Ordering");
-          let* latest_bars = Backend.latest_bars env [ "MSFT"; "NVDA" ] in
-          let msft = Bars.price latest_bars "MSFT" in
-          let nvda = Bars.price latest_bars "NVDA" in
-          let* () =
-            if msft <. nvda then Lwt.return_unit
-            else (
-              Log.app (fun k -> k "Shorting NVDA");
-              let price = nvda in
-              let order : Order.t =
-                {
-                  symbol = "NVDA";
-                  side = Side.Sell;
-                  tif = TimeInForce.Day;
-                  order_type = OrderType.Market;
-                  qty = 1;
-                }
-              in
-              let* _json_resp = Backend.create_order env order price in
-              Lwt.return_unit)
-          in
-          Lwt.return @@ continue { state with current = Listening }
-      | Finished code -> Lwt.return @@ shutdown code
+  let step (state : 'a State.t) : ('a, 'b) State.status Lwt.t =
+    let env = state.env in
+    match state.current with
+    | Initialize ->
+        Log.app (fun k -> k "Initialize");
+        Lwt.return @@ continue { state with current = Listening }
+    | Listening ->
+        let* () = Backend.Ticker.tick () in
+        Lwt.return @@ continue { state with current = Ordering }
+    | Ordering ->
+        Log.app (fun k -> k "Ordering");
+        let* latest_bars = Backend.latest_bars env [ "MSFT"; "NVDA" ] in
+        let msft = Bars.price latest_bars "MSFT" in
+        let nvda = Bars.price latest_bars "NVDA" in
+        let* () =
+          if msft <. nvda then Lwt.return_unit
+          else (
+            Log.app (fun k -> k "Shorting NVDA");
+            let order : Order.t =
+              {
+                symbol = "NVDA";
+                side = Side.Sell;
+                tif = TimeInForce.Day;
+                order_type = OrderType.Market;
+                qty = 1;
+                price = nvda;
+              }
+            in
+            let* _json_resp = Backend.create_order env order in
+            Lwt.return_unit)
+        in
+        Lwt.return @@ continue { state with current = Listening }
+    | Finished code -> Lwt.return @@ shutdown code
 
-    let run env =
-      let init = init env in
-      let rec go prev =
-        let* stepped = step prev in
-        match stepped with
-        | Running now -> go now
-        | Shutdown code -> Lwt.return code
-      in
-      go init
-  end
+  let run env =
+    let init = init env in
+    let rec go prev =
+      let* stepped = step prev in
+      match stepped with
+      | Running now -> go now
+      | Shutdown code -> Lwt.return code
+    in
+    go init
 end
