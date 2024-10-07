7fe8307fac48435adafa3d623b58a7a1607f933c
John Hester <hesterj@etableau.com>
2024-10-06 19:13:52 -0500

    chore: remove old code from binary

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,3 @@
-let _old () =
-  Fmt_tty.setup_std_outputs ();
-  let reporter = Logs_fmt.reporter () in
-  Logs.set_reporter reporter;
-  Logs.set_level ~all:true (Some Logs.Info);
-  Py.initialize ();
-  let file = "data/da.json" in
-  let json = Yojson.Safe.from_file file in
-  let _res = Longleaf.process_json json in
-  exit (if Logs.err_count () > 0 then 1 else 0)
-
 let () =
   Fmt_tty.setup_std_outputs ();
   let reporter = Logs_fmt.reporter () in
