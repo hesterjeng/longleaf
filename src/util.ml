e463169827b56eee1376adb41609e590d6ed0965
John Hester <hesterj@etableau.com>
2024-10-09 03:40:22 -0500

    code reuse

--- src/util.ml
+++ src/util.ml
@@ -9,15 +9,10 @@ include Ppx_yojson_conv_lib.Yojson_conv
 module Get_log = (val Logs.src_log Logs.(Src.create "get-log"))
 module Util_log = (val Logs.src_log Logs.(Src.create "util-log"))
 
-let get ~headers ~uri : (Yojson.Safe.t, string) Lwt_result.t =
-  let open Lwt.Syntax in
+let handle_response json response =
   let open Cohttp in
-  let open Cohttp_lwt_unix in
-  let* response, body_stream = Client.get ~headers uri in
-  let* resp_body_raw = Cohttp_lwt.Body.to_string body_stream in
-  let resp_body_json = Yojson.Safe.from_string resp_body_raw in
   match Response.status response with
-  | #Code.success_status -> Lwt.return_ok resp_body_json
+  | #Code.success_status -> Lwt.return_ok json
   | #Code.informational_status as status ->
       Get_log.err (fun k ->
           k "informational_status: %s" (Code.string_of_status status));
@@ -37,6 +32,15 @@ let get ~headers ~uri : (Yojson.Safe.t, string) Lwt_result.t =
   | `Code _ as status ->
       Get_log.err (fun k -> k "unknown code: %s" (Code.string_of_status status));
       Lwt.return_error @@ Code.string_of_status status
+
+let get ~headers ~uri : (Yojson.Safe.t, string) Lwt_result.t =
+  let open Lwt.Syntax in
+  let open Cohttp in
+  let open Cohttp_lwt_unix in
+  let* response, body_stream = Client.get ~headers uri in
+  let* resp_body_raw = Cohttp_lwt.Body.to_string body_stream in
+  let json = Yojson.Safe.from_string resp_body_raw in
+  handle_response json response
 
 let delete ~headers ~uri : (Yojson.Safe.t, string) Lwt_result.t =
   let open Lwt.Syntax in
@@ -44,28 +48,8 @@ let delete ~headers ~uri : (Yojson.Safe.t, string) Lwt_result.t =
   let open Cohttp_lwt_unix in
   let* response, body_stream = Client.delete ~headers uri in
   let* resp_body_raw = Cohttp_lwt.Body.to_string body_stream in
-  let resp_body_json = Yojson.Safe.from_string resp_body_raw in
-  match Response.status response with
-  | #Code.success_status -> Lwt.return_ok resp_body_json
-  | #Code.informational_status as status ->
-      Get_log.err (fun k ->
-          k "informational_status: %s" (Code.string_of_status status));
-      Lwt.return_error @@ Code.string_of_status status
-  | #Code.redirection_status as status ->
-      Get_log.err (fun k ->
-          k "redirection_status: %s" (Code.string_of_status status));
-      Lwt.return_error @@ Code.string_of_status status
-  | #Code.client_error_status as status ->
-      Get_log.err (fun k ->
-          k "client_error_status: %s" (Code.string_of_status status));
-      Lwt.return_error @@ Code.string_of_status status
-  | #Code.server_error_status as status ->
-      Get_log.err (fun k ->
-          k "server_error_status: %s" (Code.string_of_status status));
-      Lwt.return_error @@ Code.string_of_status status
-  | `Code _ as status ->
-      Get_log.err (fun k -> k "unknown code: %s" (Code.string_of_status status));
-      Lwt.return_error @@ Code.string_of_status status
+  let json = Yojson.Safe.from_string resp_body_raw in
+  handle_response json response
 
 let post ~headers ~body ~uri : (Yojson.Safe.t, string) Lwt_result.t =
   let open Lwt.Syntax in
@@ -73,29 +57,8 @@ let post ~headers ~body ~uri : (Yojson.Safe.t, string) Lwt_result.t =
   let open Cohttp_lwt_unix in
   let* response, body_stream = Client.post ~headers ~body uri in
   let* resp_body_raw = Cohttp_lwt.Body.to_string body_stream in
-  let resp_body_json = Yojson.Safe.from_string resp_body_raw in
-  let uri = Uri.to_string uri in
-  match Response.status response with
-  | #Code.success_status -> Lwt.return_ok resp_body_json
-  | #Code.informational_status as status ->
-      Get_log.err (fun k ->
-          k "informational_status: %s %s" (Code.string_of_status status) uri);
-      Lwt.return_error @@ Code.string_of_status status
-  | #Code.redirection_status as status ->
-      Get_log.err (fun k ->
-          k "redirection_status: %s %s" (Code.string_of_status status) uri);
-      Lwt.return_error @@ Code.string_of_status status
-  | #Code.client_error_status as status ->
-      Get_log.err (fun k ->
-          k "client_error_status: %s %s" (Code.string_of_status status) uri);
-      Lwt.return_error @@ Code.string_of_status status
-  | #Code.server_error_status as status ->
-      Get_log.err (fun k ->
-          k "server_error_status: %s %s" (Code.string_of_status status) uri);
-      Lwt.return_error @@ Code.string_of_status status
-  | `Code _ as status ->
-      Get_log.err (fun k -> k "unknown code: %s" (Code.string_of_status status));
-      Lwt.return_error @@ Code.string_of_status status
+  let json = Yojson.Safe.from_string resp_body_raw in
+  handle_response json response
 
 let get_next_page_token (x : Yojson.Safe.t) =
   Option.(
