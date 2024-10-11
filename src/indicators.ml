ce48d061df69a40b064f91e217933e6db22988ad
John Hester <hesterj@etableau.com>
2024-10-11 18:02:32 -0500

    feat: add simple moving average

--- src/indicators.ml
+++ src/indicators.ml
@@ -14,3 +14,21 @@ let accumulation_distribution_line (l : Bar_item.t list) =
       0.0 l
   in
   res
+
+let simple_moving_average (l : Bar_item.t list) =
+  let n = List.length l in
+  let close = List.map (fun (x : Bar_item.t) -> x.close) l in
+  let sma_i i =
+    let start = Int.max (i - n) 0 in
+    let range = List.range start i in
+    let vals =
+      List.map
+        (fun i ->
+          match List.get_at_idx i close with
+          | Some p -> p
+          | None -> invalid_arg "Unable to get price at index %d" i)
+        range
+    in
+    List.fold_left (fun x y -> x +. y) 0.0 vals /. Float.of_int i
+  in
+  List.mapi (fun i _ -> sma_i i) close
