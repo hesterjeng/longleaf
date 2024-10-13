79ce063e685105c37bec525df10a1eccac94506c
John Hester <hesterj@etableau.com>
2024-10-12 20:11:15 -0500

    feat: remove state_machine.ml

--- src/strategies.ml
+++ src/strategies.ml
@@ -1,4 +1,16 @@
-open State_machine
+module State = struct
+  type state =
+    [ `Initialize | `Listening | `Ordering | `Liquidate | `Finished of string ]
+
+  type 'a t = { env : Environment.t; current : state; content : 'a }
+  type ('a, 'b) status = Running of 'a t | Shutdown of 'b
+
+  let continue x = Running x
+  let shutdown x = Shutdown x
+
+  let init env =
+    { env; current = `Initialize; content = Trading_types.Bars.empty }
+end
 
 module type S = sig
   val run : Environment.t -> string Lwt.t
