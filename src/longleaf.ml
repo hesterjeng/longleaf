39e5142e2f965562d817ebd7f52e7565e30437a5
John Hester <hesterj@etableau.com>
2024-10-14 16:55:20 -0500

    feat: add positions and get_all_open_positions get request

--- src/longleaf.ml
+++ src/longleaf.ml
@@ -15,19 +15,6 @@ module Tickers = struct
     Py.List.to_array_map Py.Object.to_string ticker_symbols
 end
 
-(* let process_json (x : Yojson.Safe.t) = *)
-(*   let env = Environment.make () in *)
-(*   Log.app (fun k -> k "%a" Environment.pp env); *)
-(*   let _ = Trading_api.Accounts.get_account env in *)
-(*   let resp_body = *)
-(*     Trading_types.( *)
-(*       Lwt_main.run *)
-(*       @@ Market_data_api.Stock.historical_bars env (Timeframe.hour 1) *)
-(*            ~start:(Time.of_ymd "2024-08-28") [ "AAPL" ]) *)
-(*   in *)
-(*   Log.app (fun k -> k "resp_body longleaf: %a" Trading_types.Bars.pp resp_body); *)
-(*   Dataframe.of_json x *)
-
 let download_test env =
   let open Lwt.Syntax in
   let history_request : Market_data_api.Stock.Historical_bars_request.t =
