b251d352f8bd5a63c2b2d3b896d30b88e149dffb
John Hester <hesterj@etableau.com>
2024-10-16 19:35:04 -0500

    wip: get local minima/maxima

--- src/double_top.ml
+++ src/double_top.ml
@@ -1,5 +1,18 @@
 module Math = struct
-
   module Bar_item = Trading_types.Bars.Bar_item
 
+  type critical_point =
+    | Min of float
+    | Max of float
+
+  let local_maxima (l : Bar_item.t list) =
+    let rec aux l acc =
+      match l with
+      | [] -> List.rev acc
+      | x :: xs ->
+        let acc =
+          if
+        in
+    in
+    match l with [] | [ _ ] | [ _; _ ] -> None | _ -> Some (aux l [])
 end
