4a1da5c4fb66f21a74849f79bb6061caf9cdd045
John Hester <hesterj@etableau.com>
2024-10-24 17:56:53 -0500

    feat: ocaml 5.2

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,2 @@
 let () =
-  Fmt_tty.setup_std_outputs ();
-  let reporter = Logs_fmt.reporter () in
-  Logs.set_reporter reporter;
-  Logs.set_level ~all:true (Some Logs.Info);
-  Py.initialize ();
-  let () = Longleaf.Handler.top () in
-  exit 0
+  ()
