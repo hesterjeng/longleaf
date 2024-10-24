dd02147cfc102d3bd46ef06724e4af30cabc8448
John Hester <hesterj@etableau.com>
2024-10-23 19:42:13 -0500

    wip: trying to get button and lwt to work together

--- src/gui.ml
+++ src/gui.ml
@@ -5,25 +5,17 @@ let incr () = incr requests_sent
 let decr () = decr requests_sent
 
 let top () =
+  let open Lwt_result.Syntax in
   let set_number w i = Widget.set_text w @@ Int.to_string i in
   let count = Widget.label "0" in
-  let action _ =
-    let open Lwt.Syntax in
-    Lwt_main.run
-    @@
+  let action =
     let uri = Uri.of_string "http://localhost:8080/run_backtest" in
     let headers = Cohttp.Header.init () in
-    let* res = Util.get ~headers ~uri in
-    match res with
-    | Ok _ ->
-        incr ();
-        set_number count !requests_sent;
-        Lwt.return_unit
-    | Error e ->
-        decr ();
-        Format.printf "%s" e;
-        set_number count !requests_sent;
-        Lwt.return_unit
+    let promise = Util.get ~headers ~uri in
+    incr ();
+    set_number count !requests_sent;
+    let _, resolver = Lwt.wait () in
+    Lwt_result.return @@ fun _ -> ()
   in
   let start_button = Widget.button ~action "Start" in
   let stop_button = Widget.button "Stop" in
@@ -33,10 +25,10 @@ let top () =
       [ label; count; start_button; stop_button ]
   in
   let layout = Bogue.of_layout w in
-  Bogue.run layout
+  Lwt_result.return @@ Bogue.run layout
 
 let top () =
   try top ()
   with Unix.Unix_error (Unix.ECONNREFUSED, "connect", "") ->
-    Format.printf "Failed to connect with the server, is it running?";
-    ()
+    Format.printf "@[Failed to connect with the server, is it running?@]@.";
+    Lwt_result.return ()
