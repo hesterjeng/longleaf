1c69dcb1ed5af0b4b3eeaf1dd7b7e23c4c1f1ce7
John Hester <hesterj@etableau.com>
2024-10-25 14:25:16 -0500

    feat: serve html using dream html templates

---
+++ src/plotly.ml
@@ -0,0 +1,29 @@
+let plotly_graph_html () =
+  let plot_data =
+    `Assoc
+      [
+        ( "data",
+          `List
+            [
+              `Assoc
+                [
+                  ("x", `List [ `Float 1.0; `Float 2.0; `Float 3.0 ]);
+                  ("y", `List [ `Float 4.0; `Float 5.0; `Float 6.0 ]);
+                  ("mode", `String "lines+markers");
+                  ("name", `String "Sample Data");
+                ];
+            ] );
+        ( "layout",
+          `Assoc
+            [
+              ("title", `String "Sample Plotly Graph");
+              ("xaxis", `Assoc [ ("title", `String "X Axis") ]);
+              ("yaxis", `Assoc [ ("title", `String "Y Axis") ]);
+            ] );
+      ]
+  in
+
+  let json_data = Yojson.Basic.to_string plot_data in
+  Format.printf "@[%s@]@." json_data;
+  let plotly_html = Template.render json_data in
+  plotly_html
