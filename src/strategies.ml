5baf3db216d450a3ef64b7fbc57c44eec04a384a
John Hester <hesterj@etableau.com>
2024-10-29 15:59:10 -0500

    feat: factor out some helpers to Strategy_utils module

--- src/strategies.ml
+++ src/strategies.ml
@@ -19,68 +19,70 @@ end
 
 module Log = (val Logs.src_log Logs.(Src.create "strategies"))
 
-let check_time backtest =
-  let open CalendarLib in
-  let time = Time.now () in
-  let open_time = Calendar.Time.lmake ~hour:8 ~minute:30 () in
-  let close_time = Calendar.Time.lmake ~hour:16 () in
-  if
-    Calendar.Time.compare time open_time = 1
-    && Calendar.Time.compare time close_time = -1
-  then Lwt_result.return ()
-  else if backtest then Lwt_result.return ()
-  else
-    Lwt_result.ok
-      (Log.app (fun k -> k "Waiting because market is closed");
-       Lwt.return_unit)
+module Strategy_utils (Backend : Backend.S) = struct
+  let check_time backtest =
+    let open CalendarLib in
+    let time = Time.now () in
+    let open_time = Calendar.Time.lmake ~hour:8 ~minute:30 () in
+    let close_time = Calendar.Time.lmake ~hour:16 () in
+    if
+      Calendar.Time.compare time open_time = 1
+      && Calendar.Time.compare time close_time = -1
+    then Lwt_result.return ()
+    else if backtest then Lwt_result.return ()
+    else
+      Lwt_result.ok
+        (Log.app (fun k -> k "Waiting because market is closed");
+         Lwt.return_unit)
 
-let listen_tick backtest (tick : unit -> (unit, 'a) Lwt_result.t) =
-  let open Lwt_result.Syntax in
-  let* () = check_time backtest in
-  let* () =
-    Lwt.pick
-      [
-        (let* _ = tick () in
-         Lwt_result.return ());
-        Lwt_result.error @@ Util.listen_for_input ();
-      ]
-  in
-  Lwt_result.return ()
+  let listen_tick () =
+    let open Lwt_result.Syntax in
+    let* () = check_time Backend.is_backtest in
+    let* () =
+      Lwt.pick
+        [
+          (let* _ = Backend.Ticker.tick () in
+           Lwt_result.return ());
+          (* Lwt_result.error @@ Util.listen_for_input (); *)
+        ]
+    in
+    Lwt_result.return ()
 
-let run step env =
-  let init = State.init env in
-  let open Lwt.Syntax in
-  let rec go prev =
-    let* stepped = step prev in
-    match stepped with
-    | Ok x -> (
-        match x with
-        | State.Running now -> (go [@tailcall]) now
-        | Shutdown code -> Lwt.return code)
-    | Error s -> (
-        let try_liquidating () =
-          let liquidate = { prev with current = `Liquidate } in
-          go liquidate
-        in
-        match prev.current with
-        | `Liquidate -> Lwt.return s
-        | _ -> try_liquidating ())
-  in
-  go init
+  let run step env =
+    let init = State.init env in
+    let open Lwt.Syntax in
+    let rec go prev =
+      let* stepped = step prev in
+      match stepped with
+      | Ok x -> (
+          match x with
+          | State.Running now -> (go [@tailcall]) now
+          | Shutdown code -> Lwt.return code)
+      | Error s -> (
+          let try_liquidating () =
+            let liquidate = { prev with current = `Liquidate } in
+            go liquidate
+          in
+          match prev.current with
+          | `Liquidate -> Lwt.return s
+          | _ -> try_liquidating ())
+    in
+    go init
 
-let output_data backtest get_cash (state : _ State.t) =
-  let backtest = if backtest then "test" else "live" in
-  let json =
-    Trading_types.Bars.yojson_of_t state.content |> Yojson.Safe.to_string
-  in
-  let tail = Lots_of_words.select () ^ "_" ^ Lots_of_words.select () in
-  let filename = Format.sprintf "data/%s_%s" backtest tail in
-  let oc = open_out filename in
-  output_string oc json;
-  close_out oc;
-  Log.app (fun k -> k "cash: %f" (get_cash ()))
+  let output_data (state : _ State.t) =
+    let backtest = if Backend.is_backtest then "test" else "live" in
+    let json =
+      Trading_types.Bars.yojson_of_t state.content |> Yojson.Safe.to_string
+    in
+    let tail = Lots_of_words.select () ^ "_" ^ Lots_of_words.select () in
+    let filename = Format.sprintf "data/%s_%s" backtest tail in
+    let oc = open_out filename in
+    output_string oc json;
+    close_out oc;
+    Log.app (fun k -> k "cash: %f" (Backend.get_cash ()))
 
-let ok_code = Cohttp.Code.status_of_code 200
+  let ok_code = Cohttp.Code.status_of_code 200
+end
 
 module SimpleStateMachine (Backend : Backend.S) : S = struct
   open Trading_types
@@ -89,13 +91,15 @@ module SimpleStateMachine (Backend : Backend.S) : S = struct
 
   let shutdown = Backend.shutdown
 
+  module SU = Strategy_utils (Backend)
+
   let step (state : 'a State.t) : (('a, 'b) State.status, string) Lwt_result.t =
     let env = state.env in
     match state.current with
     | `Initialize ->
         Lwt_result.return @@ State.continue { state with current = `Listening }
     | `Listening ->
-        let* () = listen_tick Backend.backtesting Backend.Ticker.tick in
+        let* () = SU.listen_tick () in
         Lwt_result.return @@ State.continue { state with current = `Ordering }
     | `Liquidate ->
         let* _ = Backend.liquidate env in
@@ -103,7 +107,7 @@ module SimpleStateMachine (Backend : Backend.S) : S = struct
         @@ State.continue
              { state with current = `Finished "Successfully liquidated" }
     | `Finished code ->
-        output_data Backend.backtesting Backend.get_cash state;
+        SU.output_data state;
         Lwt_result.return @@ State.shutdown code
     | `Ordering ->
         let* latest_bars = Backend.latest_bars env [ "MSFT"; "NVDA" ] in
@@ -140,5 +144,5 @@ module SimpleStateMachine (Backend : Backend.S) : S = struct
         @@ State.continue
              { state with current = `Listening; content = new_bars }
 
-  let run = run step
+  let run = SU.run step
 end
