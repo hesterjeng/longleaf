f1360bcf8f6a8fd953f43ad87a1149759e5cea31
John Hester <hesterj@etableau.com>
2024-10-30 16:29:37 -0500

    wip: use piaf with eio

--- bin/server.ml
+++ bin/server.ml
@@ -5,5 +5,3 @@ let _ =
   Logs.set_level ~all:true (Some Logs.Info);
   Eio_main.run @@ fun eio_env ->
   Eio.Switch.run (fun sw -> Longleaf.top sw eio_env)
-(* Lwt_eio.with_event_loop ~clock:eio_env#clock ~debug:true @@ fun () -> *)
-(* Longleaf.top eio_env *)
