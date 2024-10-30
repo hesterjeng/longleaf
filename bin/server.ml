d3ce1ad0848fe2a0b415cde2d5018b0335c59f1f
John Hester <hesterj@etableau.com>
2024-10-30 01:52:32 -0500

    feat: eio in server

--- bin/server.ml
+++ bin/server.ml
@@ -6,9 +6,11 @@ let _ =
   let reporter = Logs_fmt.reporter () in
   Logs.set_reporter reporter;
   Logs.set_level ~all:true (Some Logs.Info);
-  Eio_main.run @@ fun env ->
-  Lwt_eio.with_event_loop ~clock:env#clock @@ fun _ ->
-  Lwt_eio.run_lwt @@ fun () -> Longleaf.top
+  Eio_main.run @@ fun eio_env -> Longleaf.top eio_env
+(* Eio_main.run @@ fun env -> *)
+(* Lwt_eio.with_event_loop ~clock:env#clock @@ fun _ -> *)
+(* Lwt_eio.run_lwt @@ fun () -> Longleaf.top *)
+
 (* Eio_main.run @@ fun env -> *)
 (* Lwt_eio.with_event_loop ~clock:env#clock @@ fun _ -> *)
 (* Lwt_eio.run_lwt @@ fun () -> Dream.serve ~port:8080 @@ Longleaf.Handler.top () *)
