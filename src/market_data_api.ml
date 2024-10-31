321b309c31b312b94d5ceeb8f8b2d0bd8489f87c
John Hester <hesterj@etableau.com>
2024-10-31 03:55:53 -0500

    feat: piaf market data api

--- src/market_data_api.ml
+++ src/market_data_api.ml
@@ -1,8 +1,19 @@
 open Trading_types
+module Headers = Piaf.Headers
 
 module Make (Alpaca : Util.ALPACA_SERVER) = struct
-  let h = Trading_api.h
   let client = Alpaca.client
+  let longleaf_env = Alpaca.longleaf_env
+  let get = Util.get_piaf ~client
+  let delete = Util.delete_piaf ~client
+  let post = Util.post_piaf ~client
+
+  let headers () =
+    Headers.of_list
+      [
+        ("APCA-API-KEY-ID", longleaf_env.apca_api_key_id);
+        ("APCA-API-SECRET-KEY", longleaf_env.apca_api_secret_key);
+      ]
 
   module Stock = struct
     module Historical_bars_request = struct
@@ -14,61 +25,42 @@ module Make (Alpaca : Util.ALPACA_SERVER) = struct
       [@@deriving show, yojson]
     end
 
-    let historical_auctions (env : Environment.t) (symbols : string list) =
-      let headers = h env in
-      let symbols = String.concat "," symbols in
-      let uri = Uri.with_path env.apca_api_data_url "/v2/stocks/auctions" in
-      let body =
-        `Assoc [ ("symbols", `String symbols) ]
-        |> Yojson.Safe.to_string |> Cohttp_lwt.Body.of_string
-      in
-      Util.post ~headers ~body ~uri
-
-    let historical_bars (env : Environment.t)
-        (request : Historical_bars_request.t) =
-      let headers = h env in
+    let historical_bars () (request : Historical_bars_request.t) =
       let symbols = String.concat "," request.symbols in
-      let uri =
-        Uri.with_path env.apca_api_data_url "/v2/stocks/bars" |> fun u ->
-        Uri.add_query_param' u ("symbols", symbols) |> fun u ->
-        Uri.add_query_param' u
-          ("timeframe", Timeframe.to_string request.timeframe)
-        |> fun u ->
-        Uri.add_query_param' u ("start", Ptime.to_rfc3339 request.start)
+      let endpoint = "/v2/stocks/bars" in
+      let headers =
+        headers () |> fun h ->
+        Headers.add_list h
+          [
+            ("symbols", symbols);
+            ("timeframe", Timeframe.to_string request.timeframe);
+            ("start", Ptime.to_rfc3339 request.start);
+          ]
       in
-      let rec collect_data uri acc =
-        let resp_body_json = Util.get ~headers ~uri in
+      let rec collect_data ~endpoint ~headers acc =
+        let resp_body_json = get ~headers ~endpoint in
         let acc = Bars.t_of_yojson resp_body_json :: acc in
         match Util.get_next_page_token resp_body_json with
         | Some npt ->
-            let uri =
-              let removed = Uri.remove_query_param uri "page_token" in
-              Uri.add_query_param' removed ("page_token", npt)
-            in
-            collect_data uri acc
+            let headers = Headers.replace headers "page_token" npt in
+            collect_data ~endpoint ~headers acc
         | None -> acc
       in
-      let paginated = collect_data uri [] in
+      let paginated = collect_data ~endpoint ~headers [] in
       Bars.combine paginated
 
-    let latest_bars (env : Environment.t) (symbols : string list) =
-      let headers = h env in
+    let latest_bars () (symbols : string list) =
       let symbols = String.concat "," symbols in
-      let uri =
-        Uri.with_path env.apca_api_data_url "/v2/stocks/bars/latest" |> fun u ->
-        Uri.add_query_param' u ("symbols", symbols)
-      in
-      let resp_body_json = Util.get ~headers ~uri in
+      let endpoint = "/v2/stocks/bars/latest" in
+      let headers = headers () |> fun h -> Headers.add h "symbols" symbols in
+      let resp_body_json = get ~headers ~endpoint in
       Bars.t_of_yojson resp_body_json
 
-    let latest_quotes (env : Environment.t) (symbols : string list) =
-      let headers = h env in
+    let latest_quotes () (symbols : string list) =
       let symbols = String.concat "," symbols in
-      let uri =
-        Uri.with_path env.apca_api_data_url "/v2/stocks/quotes/latest"
-        |> fun u -> Uri.add_query_param' u ("symbols", symbols)
-      in
-      let resp_body_json = Util.get ~headers ~uri in
+      let endpoint = "/v2/stocks/quotes/latest" in
+      let headers = headers () |> fun h -> Headers.add h "symbols" symbols in
+      let resp_body_json = get ~headers ~endpoint in
       resp_body_json
   end
 end
