faa9841a5b1463edec3badfe6d3879fe5c23b657
John Hester <hesterj@etableau.com>
2024-11-07 13:16:01 -0600

    feat: anthropic api, add a stop loss order in double top

--- src/llm.ml
+++ src/llm.ml
@@ -37,3 +37,43 @@ module OpenAI = struct
         | Error e -> invalid_arg @@ Piaf.Error.to_string e)
     | Error e -> invalid_arg @@ Piaf.Error.to_string e
 end
+
+module Anthropic = struct
+  let create_request_body prompt =
+    `Assoc
+      [
+        ("model", `String "claude-3-5-sonnet-20241022");
+        ( "messages",
+          `List
+            [ `Assoc [ ("role", `String "user"); ("content", `String prompt) ] ]
+        );
+        ("max_tokens", `Int 1024);
+      ]
+    |> Yojson.Safe.to_string |> Piaf.Body.of_string
+
+  (* let handle_response (response : Yojson.Safe.t) = *)
+  (*   match response with *)
+  (*   | *)
+
+  let chat ~sw ~env (content : string) =
+    let openai_key = Unix.getenv "ANTHROPIC_KEY" in
+    let uri = Uri.of_string "https://api.anthropic.com/v1/messages" in
+    let headers =
+      Headers.of_list
+        [
+          ("Content-Type", "application/json");
+          ("x-api-key", openai_key);
+          ("anthropic_version", "2023-06-01");
+        ]
+      |> Headers.to_list
+    in
+    let body = create_request_body content in
+    match Piaf.Client.Oneshot.post ~headers ~body ~sw env uri with
+    | Ok { body; _ } -> (
+        Yojson.Safe.from_string
+        @@
+        match Piaf.Body.to_string body with
+        | Ok body_str -> body_str
+        | Error e -> invalid_arg @@ Piaf.Error.to_string e)
+    | Error e -> invalid_arg @@ Piaf.Error.to_string e
+end
