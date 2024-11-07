6e0ecc4b9f69bd01c14754e1db8efc6ca66bd250
John Hester <hesterj@etableau.com>
2024-11-07 12:06:59 -0600

    wip: use openai api

--- src/llm.ml
+++ src/llm.ml
@@ -1,33 +1,39 @@
 module Headers = Piaf.Headers
 
-let create_request_body prompt =
-  `Assoc
-    [
-      ("model", `String "gpt-4");
-      ( "messages",
-        `List
-          [ `Assoc [ ("role", `String "user"); ("content", `String prompt) ] ]
-      );
-    ]
-  |> Yojson.Safe.to_string |> Piaf.Body.of_string
-
-let chat ~sw ~env (content : string) =
-  let openai_key = Unix.getenv "OPENAI_KEY" in
-  let uri = Uri.of_string "https://api.openai.com/v1/chat/completions" in
-  let headers =
-    Headers.of_list
+module OpenAI = struct
+  let create_request_body prompt =
+    `Assoc
       [
-        ("Content-Type", "application/json");
-        ("Authorization", "Bearer " ^ openai_key);
+        ("model", `String "gpt-3.5-turbo");
+        ( "messages",
+          `List
+            [ `Assoc [ ("role", `String "user"); ("content", `String prompt) ] ]
+        );
       ]
-    |> Headers.to_list
-  in
-  let body = create_request_body content in
-  match Piaf.Client.Oneshot.post ~headers ~body ~sw env uri with
-  | Ok { body; _ } -> (
-      Yojson.Safe.from_string
-      @@
-      match Piaf.Body.to_string body with
-      | Ok body_str -> body_str
-      | Error e -> invalid_arg @@ Piaf.Error.to_string e)
-  | Error e -> invalid_arg @@ Piaf.Error.to_string e
+    |> Yojson.Safe.to_string |> Piaf.Body.of_string
+
+  (* let handle_response (response : Yojson.Safe.t) = *)
+  (*   match response with *)
+  (*   | *)
+
+  let chat ~sw ~env (content : string) =
+    let openai_key = Unix.getenv "OPENAI_KEY" in
+    let uri = Uri.of_string "https://api.openai.com/v1/chat/completions" in
+    let headers =
+      Headers.of_list
+        [
+          ("Content-Type", "application/json");
+          ("Authorization", "Bearer " ^ openai_key);
+        ]
+      |> Headers.to_list
+    in
+    let body = create_request_body content in
+    match Piaf.Client.Oneshot.post ~headers ~body ~sw env uri with
+    | Ok { body; _ } -> (
+        Yojson.Safe.from_string
+        @@
+        match Piaf.Body.to_string body with
+        | Ok body_str -> body_str
+        | Error e -> invalid_arg @@ Piaf.Error.to_string e)
+    | Error e -> invalid_arg @@ Piaf.Error.to_string e
+end
