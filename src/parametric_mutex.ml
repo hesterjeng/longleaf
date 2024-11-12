db0a41a2a21284719ade5c395c94a1be68ae5007
John Hester <hesterj@etableau.com>
2024-11-11 18:28:13 -0600

    wip: use new mutex in main file

--- src/parametric_mutex.ml
+++
@@ -1,8 +0,0 @@
-type 'a t = { mutable data : 'a; mutex : Eio.Mutex.t }
-
-let make x = { data = x; mutex = Eio.Mutex.create () }
-
-let set mut value =
-  Eio.Mutex.use_rw ~protect:true mut.mutex @@ fun () -> mut.data <- value
-
-let get mut = Eio.Mutex.use_ro mut.mutex @@ fun () -> mut.data
