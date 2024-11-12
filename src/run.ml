fb8fa1952525b658f178b734b7c50530f8158fce
John Hester <hesterj@etableau.com>
2024-11-12 15:56:20 -0600

    feat: try to create a reusable functor structure for new strategies

--- src/run.ml
+++ src/run.ml
@@ -1,17 +1,57 @@
-module DoubleTopRun (Input : sig
+module type RUN_DATA = sig
+  val bars : Bars.t
+  val symbols : string list
+  val tick : float
+end
+
+module type RUN_CONTEXT = sig
   val eio_env : Eio_unix.Stdenv.base
   val longleaf_env : Environment.t
   val switch : Eio.Switch.t
-end) (LongleafMutex : Backend.LONGLEAF_MUTEX)
-=
-struct
-  module Ingredients : Backend.BACKEND_INPUT = struct
-    module LongleafMutex = LongleafMutex
 
-    let eio_env = Input.eio_env
-    let longleaf_env = Input.longleaf_env
-    let switch = Input.switch
+  (* module StrategyBuilder : Strategies.STRAT_BUILDER *)
+end
 
+module type S = sig
+  val top : bool -> unit
+end
+
+module Make
+    (Data : RUN_DATA)
+    (StrategyBuilder : Strategies.STRAT_BUILDER)
+    (LongleafMutex : Backend.LONGLEAF_MUTEX)
+    (Context : RUN_CONTEXT) : S = struct
+  module Input = struct
+    include Data
+    include Context
+  end
+
+  module Backtesting () : Backend.S =
+    Backend.Backtesting (Input) (LongleafMutex)
+
+  module Alpaca () : Backend.S =
+    Backend.Alpaca
+      (Input)
+      (Ticker.Make (struct
+        let time = Input.tick
+      end))
+      (LongleafMutex)
+
+  let top backtesting =
+    let backend =
+      if backtesting then (module Backtesting () : Backend.S)
+      else (module Alpaca () : Backend.S)
+    in
+    let module Backend = (val backend) in
+    let module Strategy = StrategyBuilder (Backend) in
+    let res = Strategy.run () in
+    Backend.shutdown ();
+    Eio.traceln "Result: %s" res;
+    ()
+end
+
+module DoubleTop = struct
+  module Data : RUN_DATA = struct
     let bars =
       Yojson.Safe.from_file "data/download_forcing_Dardistan"
       |> Bars.t_of_yojson
@@ -37,26 +77,9 @@ struct
         "UBER";
         "LLY";
       ]
+
+    let tick = 60.0
   end
 
-  module Backtesting () : Backend.S = Backend.Backtesting (Ingredients)
-
-  module Alpaca () : Backend.S =
-    Backend.Alpaca
-      (Ingredients)
-      (Ticker.Make (struct
-        let time = 60.0
-      end))
-
-  let top backtesting =
-    let backend =
-      if backtesting then (module Backtesting () : Backend.S)
-      else (module Alpaca () : Backend.S)
-    in
-    let module Backend = (val backend) in
-    let module Strategy = Double_top.DoubleTop (Backend) in
-    let res = Strategy.run () in
-    Backend.shutdown ();
-    Eio.traceln "Result: %s" res;
-    ()
+  module Make = Make (Data) (Double_top.DoubleTop)
 end
