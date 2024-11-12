a22b84c3eb8b244793dbc468ebf4ce141fc9e3e8
John Hester <hesterj@etableau.com>
2024-11-12 14:10:25 -0600

    feat: add a functor type, backtesting arg

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,7 @@
 let _ =
+  let backtesting = true in
   Fmt_tty.setup_std_outputs ();
   let reporter = Logs_fmt.reporter () in
   Logs.set_reporter reporter;
   Logs.set_level ~all:true (Some Logs.Info);
-  Eio_main.run @@ fun eio_env -> Longleaf.top eio_env
+  Eio_main.run @@ fun eio_env -> Longleaf.top eio_env backtesting
