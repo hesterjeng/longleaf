27a99715b034d35badf211950d7be7e7dce5601d
John Hester <hesterj@etableau.com>
2024-11-15 18:06:06 -0600

    feat: format

--- src/javascript/backtest_button.js
+++ src/javascript/backtest_button.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector("#backtest_button button");
 
   button.addEventListener("click", () => {
-    fetch('http://localhost:8080/run_dead', { method: 'GET' })
-      .then(response => {
+    fetch("http://localhost:8080/run_dead", { method: "GET" })
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json(); // or .text() for plain text response
       })
-      .then(data => {
+      .then((data) => {
         console.log(data); // Handle the data received from the server
-        button.innerText = 'Backtest Started'
+        button.innerText = "Backtest Started";
       })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
       });
   });
 });
