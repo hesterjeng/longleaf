9420910bb04ac8dd04935edb196342a6c63cc8ae
John Hester <hesterj@etableau.com>
2024-11-18 15:16:22 -0600

    wip: use cmdliner

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,17 @@
-let _ =
+module Args = struct
+  (* Define the CLI arguments *)
+  let runtype =
+    let doc = "The type of run." in
+    Cmdliner.Arg.(
+      required & pos 0 (some float) None & info [] ~docv:"WIDTH" ~doc)
+
+  let height_arg =
+    let doc = "The height of the rectangle." in
+    Cmdliner.Arg.(
+      required & pos 1 (some float) None & info [] ~docv:"HEIGHT" ~doc)
+end
+
+let run runtype output_file =
   let backtesting = true in
   Fmt_tty.setup_std_outputs ();
   let reporter = Logs_fmt.reporter () in
