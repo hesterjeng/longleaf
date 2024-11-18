73599bc6b10682318ea2d3cbce1bbb554800882f
John Hester <hesterj@etableau.com>
2024-11-18 15:08:26 -0600

    feat: cmdliner helpers for runtype

--- src/options.ml
+++ src/options.ml
@@ -1,10 +1,11 @@
-type runtype =
-  | Live
-  | Paper
-  | Backtest
+type runtype = Live | Paper | Backtest [@@deriving show]
+type t = { runtype : runtype; output_file : string option } [@@deriving show]
 
-type t =
-  {
-    runtype : runtype;
-    output_file : string option;
-  }
+let runtype_of_string_res x =
+  match x with
+  | "Live" -> Ok Live
+  | "Paper" -> Ok Paper
+  | "Backtest" -> Ok Backtest
+  | _ -> Error (`Msg "Expected a valid runtype")
+
+let runtype_conv = Cmdliner.Arg.conv (runtype_of_string_res,pp_runtype)
