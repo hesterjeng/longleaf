d788fea46b0874c4e20d67b6e8ed4723f55d8bbb
John Hester <hesterj@etableau.com>
2024-11-19 14:48:25 -0600

    chore: helpful messages

--- bin/shutdown.ml
+++ bin/shutdown.ml
@@ -1,13 +1,14 @@
 let () =
   Eio_main.run @@ fun env ->
   Eio.Switch.run @@ fun sw ->
+  Eio.traceln "@[Preparing to send shutdown command.@]@.";
   let uri = Uri.of_string "http://localhost:8080/shutdown" in
   match Piaf.Client.Oneshot.get ~sw env uri with
   | Ok r ->
       Eio.traceln "@[%a@]@." Piaf.Response.pp_hum r;
-      Eio.traceln "@[Shutdown command sent@]@.";
+      Eio.traceln "@[Shutdown command sent.@]@.";
       ()
   | Error e ->
       Eio.traceln "@[%a@]@." Piaf.Error.pp_hum e;
-      Eio.traceln "@[Error while sending shutdown command]@.";
+      Eio.traceln "@[Error while sending shutdown command.]@.";
       ()
