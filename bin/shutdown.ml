460e24cce99956237e00fb03425a4cea86c36e5e
John Hester <hesterj@etableau.com>
2024-11-19 14:11:04 -0600

    feat: add bin/shutdown.ml

---
+++ bin/shutdown.ml
@@ -0,0 +1,13 @@
+let () =
+  Eio_main.run @@ fun env ->
+  Eio.Switch.run @@ fun sw ->
+  let uri = Uri.of_string "http://localhost:8080/shutdown" in
+  match Piaf.Client.Oneshot.get ~sw env uri with
+  | Ok r ->
+      Eio.traceln "@[%a@]@." Piaf.Response.pp_hum r;
+      Eio.traceln "@[Shutdown command sent@]@.";
+      ()
+  | Error e ->
+      Eio.traceln "@[%a@]@." Piaf.Error.pp_hum e;
+      Eio.traceln "@[Error while sending shutdown command]@.";
+      ()
