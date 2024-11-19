2d51bd05cfb14dcedfbea91d54cd31ddd6505254
John Hester <hesterj@etableau.com>
2024-11-18 23:34:17 -0600

    feat: add an optional string arg for an output file

--- bin/main.ml
+++ bin/main.ml
@@ -6,26 +6,25 @@ module Args = struct
     Cmdliner.Arg.(
       required & pos 0 (some runtype_conv) None & info [] ~docv:"runtype" ~doc)
 
-  (* let output_file_arg = *)
-  (*   let doc = "The height of the rectangle." in *)
-  (*   Cmdliner.Arg.( *)
-  (*     val (pos 1 (non_dir_file)) *)
-  (*   ) *)
-  (* Cmdliner.Arg.( *)
-  (* opt & pos 1 (some string) None & info [] ~docv:"HEIGHT" ~doc) *)
+  let output_file_arg =
+    let doc = "Output file for a log." in
+    Cmdliner.Arg.(value & opt (some string) None & info [ "o"; "output" ] ~doc)
 end
 
 module Cmd = struct
-  let run runtype =
+  let run runtype output =
     let backtesting = true in
     Fmt_tty.setup_std_outputs ();
     let reporter = Logs_fmt.reporter () in
     Logs.set_reporter reporter;
     Logs.set_level ~all:true (Some Logs.Info);
-    Eio_main.run @@ fun eio_env -> Longleaf.top ~runtype eio_env backtesting
+    Eio_main.run @@ fun eio_env ->
+    Longleaf.top ~runtype ~output eio_env backtesting
 
   let top =
-    let term = Cmdliner.Term.(const run $ Args.runtype_arg) in
+    let term =
+      Cmdliner.Term.(const run $ Args.runtype_arg $ Args.output_file_arg)
+    in
     let info = Cmdliner.Cmd.info "longleaf" in
     Cmdliner.Cmd.v info term
 end
