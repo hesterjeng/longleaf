c68baca5eab72dfa0dbeb595dfa5c696d78e9eb4
John Hester <hesterj@etableau.com>
2024-11-20 01:04:31 -0600

    feat: helpful failure if environment variables fail, opam update

--- src/environment.ml
+++ src/environment.ml
@@ -8,15 +8,20 @@ type t = {
 [@@deriving show]
 
 let make () =
-  let apca_api_key_id = Unix.getenv "APCA_API_KEY_ID" in
-  let apca_api_secret_key = Unix.getenv "APCA_API_SECRET_KEY" in
-  let openai_key = Unix.getenv "OPENAI_KEY" in
-  let apca_api_base_url = Unix.getenv "APCA_API_BASE_URL" |> Uri.of_string in
-  let apca_api_data_url = Unix.getenv "APCA_API_DATA_URL" |> Uri.of_string in
-  {
-    apca_api_key_id;
-    apca_api_secret_key;
-    apca_api_base_url;
-    apca_api_data_url;
-    openai_key;
-  }
+  try
+    let apca_api_key_id = Unix.getenv "APCA_API_KEY_ID" in
+    let apca_api_secret_key = Unix.getenv "APCA_API_SECRET_KEY" in
+    let openai_key = Unix.getenv "OPENAI_KEY" in
+    let apca_api_base_url = Unix.getenv "APCA_API_BASE_URL" |> Uri.of_string in
+    let apca_api_data_url = Unix.getenv "APCA_API_DATA_URL" |> Uri.of_string in
+    {
+      apca_api_key_id;
+      apca_api_secret_key;
+      apca_api_base_url;
+      apca_api_data_url;
+      openai_key;
+    }
+  with Not_found ->
+    Eio.traceln
+      "@[Could not find an environment variable.  Is the .envrc configured?@]@.";
+    exit 1
