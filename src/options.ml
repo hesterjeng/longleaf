cfc7419cb4464020dea55ded54af9deeacd27bde
John Hester <hesterj@etableau.com>
2024-11-24 16:50:15 -0600

    feat: move tiingo client creation to appropriate file

--- src/options.ml
+++ src/options.ml
@@ -7,9 +7,11 @@ module Runtype = struct
     | "Paper" | "paper" -> Ok Paper
     | "Backtest" | "backtest" -> Ok Backtest
     | "Manual" | "manual" -> Ok Backtest
-    | _ -> Error (`Msg "Expected a valid runtype") [@@deriving eq]
+    | _ -> Error (`Msg "Expected a valid runtype")
+  [@@deriving eq]
 
   let conv = Cmdliner.Arg.conv (of_string_res, pp)
+  let is_manual = function Manual -> true | _ -> false
 end
 
 type t = { runtype : Runtype.t; output_file : string option } [@@deriving show]
