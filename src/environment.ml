b94d4d7cba63f31dac7541c1cbe72963f230027a
John Hester <hesterj@etableau.com>
2024-11-24 12:23:30 -0600

    feat: polygon key

--- src/environment.ml
+++ src/environment.ml
@@ -2,6 +2,7 @@ type t = {
   apca_api_key_id : string;
   apca_api_secret_key : string;
   openai_key : string;
+  polygon_key : string;
   apca_api_base_url : Uri.t;
   apca_api_data_url : Uri.t;
 }
@@ -12,6 +13,7 @@ let make () =
     let apca_api_key_id = Unix.getenv "APCA_API_KEY_ID" in
     let apca_api_secret_key = Unix.getenv "APCA_API_SECRET_KEY" in
     let openai_key = Unix.getenv "OPENAI_KEY" in
+    let polygon_key = Unix.getenv "POLYGON_KEY" in
     let apca_api_base_url = Unix.getenv "APCA_API_BASE_URL" |> Uri.of_string in
     let apca_api_data_url = Unix.getenv "APCA_API_DATA_URL" |> Uri.of_string in
     {
@@ -20,6 +22,7 @@ let make () =
       apca_api_base_url;
       apca_api_data_url;
       openai_key;
+      polygon_key;
     }
   with Not_found ->
     Eio.traceln
