663eb0a6059c5a93c032ec0934a1c61dcfbd5e2f
John Hester <hesterj@etableau.com>
2024-11-24 16:42:34 -0600

    feat: add option for manual call testing

--- src/options.ml
+++ src/options.ml
@@ -1,11 +1,12 @@
 module Runtype = struct
-  type t = Live | Paper | Backtest [@@deriving show]
+  type t = Live | Paper | Backtest | Manual [@@deriving show]
 
   let of_string_res x =
     match x with
     | "Live" | "live" -> Ok Live
     | "Paper" | "paper" -> Ok Paper
     | "Backtest" | "backtest" -> Ok Backtest
+    | "Manual" | "manual" -> Ok Backtest
     | _ -> Error (`Msg "Expected a valid runtype")
 
   let conv = Cmdliner.Arg.conv (of_string_res, pp)
