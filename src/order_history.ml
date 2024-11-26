2a4783c74f5eca5f75fcd68f59e23a41c69cdd45
John Hester <hesterj@etableau.com>
2024-11-25 19:43:24 -0600

    feat: enable orders endpoint

--- src/order_history.ml
+++
@@ -1,8 +0,0 @@
-module Order = Trading_types.Order
-
-type order_history = (Time.t * Order.t) list [@@deriving show, yojson]
-type t = (Time.t, Order.t) Hashtbl.t
-
-let yojson_of_t (x : t) =
-  let l = Hashtbl.to_list x in
-  yojson_of_order_history l
