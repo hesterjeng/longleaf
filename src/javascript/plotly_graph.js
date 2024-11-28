5140147be6832d82b5b3e158e7cd1445cca8a066
John Hester <hesterj@etableau.com>
2024-11-28 17:26:05 -0600

    wip: render all graphs

--- src/javascript/plotly_graph.js
+++ src/javascript/plotly_graph.js
@@ -1,5 +1,5 @@
 // Function to fetch data and update the graph
-export async function fetchAndRender(endPoint) {
+export async function fetchAndRender(div, endPoint) {
                 try {
     const endpointUrl = "http://localhost:8080/" + endPoint;
     const response = await fetch(endpointUrl);
@@ -64,14 +64,31 @@ export async function fetchAndRender(endPoint) {
     };
 
     const layout = {
-      title: "Dynamic Plotly Graph",
+      title: endPoint,
       xaxis: { title: "X-axis", type: "category", tickmode: "linear", dtick: 20, showticklabels: false,},
       yaxis: { title: "Y-axis" },
     };
 
     // Render the graph (use Plotly.react for updates)
-    Plotly.react("graph", [trace, buyTrace, sellTrace], layout);
+    Plotly.react(div, [trace, buyTrace, sellTrace], layout);
   } catch (error) {
     console.error("Error AFTER fetching:", error);
+  }
+}
+
+export async function fetchAndRenderAll(symbols) {
+  try {
+  const symbols_array = symbols.split(',');
+  const length = symbols.length;
+  const container = document.getElementById("graph");
+  for (let i = 0; i < length; i++) {
+    const symbol = symbols_array[i];
+    const result = fetchAndRender(symbol);
+    const newDiv = document.createElement("div");
+    newDiv.id = symbol;
+    container.appendChild(newDiv);
+}
+
+
   }
 }
