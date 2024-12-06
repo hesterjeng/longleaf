181070556c7162ea7b45018bfd713539f88355f1
John Hester <hesterj@etableau.com>
2024-12-05 18:51:38 -0600

    feat: improvements to data downloader

--- bin/data_downloader.ml
+++ bin/data_downloader.ml
@@ -1,5 +1,12 @@
 open Longleaf
 
+module Args = struct
+  (* Define the CLI arguments *)
+  let today_arg =
+    let doc = "Download data only for today." in
+    Cmdliner.Arg.(value & flag & info [ "t" ] ~doc)
+end
+
 let some_symbols =
   [
     "NVDA";
@@ -21,6 +28,14 @@ let some_symbols =
     "UBER";
     "LLY";
   ]
+
+let get_todays_date () =
+  let time = Unix.time () in
+  let local_time = Unix.localtime time in
+  Printf.sprintf "%04d-%02d-%02d"
+    (1900 + local_time.Unix.tm_year) (* Year *)
+    (local_time.Unix.tm_mon + 1) (* Month *)
+    local_time.Unix.tm_mday (* Day *)
 
 module Downloader = struct
   let data_client switch eio_env (longleaf_env : Longleaf.Environment.t) =
@@ -51,23 +66,35 @@ module Downloader = struct
     let str = Yojson.Safe.to_string bars_json in
     let tail = Lots_of_words.select () ^ "_" ^ Lots_of_words.select () in
     let filename = Format.sprintf "data/download_%s.json" tail in
+    Eio.traceln "@[%s@]@." filename;
     let oc = open_out filename in
     output_string oc str;
     close_out oc;
     Piaf.Client.shutdown data_client
 end
 
-let _ =
-  Fmt_tty.setup_std_outputs ();
-  (* let reporter = Logs_fmt.reporter () in *)
-  (* Logs.set_reporter reporter; *)
-  (* Logs.set_level ~all:true (Some Logs.Info); *)
-  let request : Longleaf.Market_data_api.Historical_bars_request.t =
-    {
-      timeframe = Trading_types.Timeframe.min 10;
-      start = Time.of_ymd "2024-11-01";
-      symbols = some_symbols;
-    }
-  in
-  let _ = Eio_main.run @@ fun eio_env -> Downloader.top eio_env request in
-  ()
+module Cmd = struct
+  let run today =
+    Fmt_tty.setup_std_outputs ();
+    let start =
+      if today then Time.of_ymd @@ get_todays_date ()
+      else Time.of_ymd "2024-11-01"
+    in
+    let request : Longleaf.Market_data_api.Historical_bars_request.t =
+      {
+        timeframe = Trading_types.Timeframe.min 10;
+        start;
+        symbols = some_symbols;
+      }
+    in
+    let _ = Eio_main.run @@ fun eio_env -> Downloader.top eio_env request in
+    ()
+
+  let top =
+    let term = Cmdliner.Term.(const run $ Args.today_arg) in
+    let doc = "Simple data downloader." in
+    let info = Cmdliner.Cmd.info ~doc "./main.exe" in
+    Cmdliner.Cmd.v info term
+end
+
+let () = Stdlib.exit @@ Cmdliner.Cmd.eval Cmd.top
