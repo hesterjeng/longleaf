b914280b4b8936ab0903c649af472e359e127916
John Hester <hesterj@etableau.com>
2024-12-17 03:46:05 -0600

    feat: play with lisp

--- elisp/top.el
+++ elisp/top.el
@@ -1,5 +1,6 @@
 (require 'general)
 
+<<<<<<< HEAD
 (defvar preload-file "No preload selected")
 (setq preload-file "No preload selected")
 (defvar target-file "No target selected")
@@ -153,3 +154,33 @@
         :desc "do something" "a" #'do-something
         )
        ))
+=======
+(defun longleaf-backtest ()
+  "Display a custom menu for selecting a file and running a command in vterm."
+  (interactive)
+  (let
+      ((preload-file (read-file-name "Select a preload file: ")))
+    (let
+      ((target-file (read-file-name "Select a target file: ")))
+
+    ;; Create or get an existing vterm buffer
+    (let ((vterm-buffer (get-buffer-create "*vterm*")))
+      (with-current-buffer vterm-buffer
+        ;; Start vterm if it's not already running
+        (unless (eq major-mode 'vterm-mode)
+          (vterm))  ;; Start vterm in the buffer if not already running
+        ;; Send the shell command to vterm
+        (vterm-send-string
+         (concat "cd ..;./main.exe backtest --preload " preload-file " --target " target-file))
+        (vterm-send-return))  ;; Simulate pressing Enter
+
+      ;; Switch to the vterm buffer
+      (switch-to-buffer vterm-buffer)
+      (message "Running command in vterm: %s"
+               (concat "dune exec bin/main.exe backtest --preload " preload-file " --target " target-file))))))
+
+
+
+(map! :leader
+      :desc "Longleaf Backtest" "l" #'longleaf-backtest)
+>>>>>>> 814de69 (feat: play with lisp)
