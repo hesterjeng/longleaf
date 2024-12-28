0fd37b2adec01d9c7d6ad01a2a5b9f8758882ef9
John Hester <hesterj@etableau.com>
2024-12-28 14:11:39 -0600

    feat: Order_history.find, Stats.add_orders

--- src/stats.ml
+++ src/stats.ml
@@ -9,3 +9,10 @@ let empty = []
 let append (x : item) (l : t) = x :: l
 let compare x y = Ptime.compare x.time y.time
 let sort (x : t) = List.sort compare x
+
+let add_orders (orders : Order_history.t) (x : t) =
+  List.map
+    (fun item ->
+      let time = item.time in
+      { item with order = Order_history.find time orders })
+    x
