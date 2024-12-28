f1464ab1c30549ece31ce02b298256773f03c213
John Hester <hesterj@etableau.com>
2024-12-27 22:24:39 -0600

    feat: sort stats before sending them

--- src/gui.ml
+++ src/gui.ml
@@ -65,7 +65,9 @@ let connection_handler ~(mutices : mutices) (params : Request_info.t Server.ctx)
       Response.of_string ~body `OK
   | { Request.meth = `GET; target = "/stats"; _ } ->
       let stats = Pmutex.get mutices.stats_mutex in
-      let body = Stats.yojson_of_t stats |> Yojson.Safe.to_string in
+      let body =
+        Stats.sort stats |> Stats.yojson_of_t |> Yojson.Safe.to_string
+      in
       Response.of_string ~body `OK
   | { Request.meth = `GET; target = "/graphs_json"; _ } ->
       let bars = Pmutex.get mutices.data_mutex in
