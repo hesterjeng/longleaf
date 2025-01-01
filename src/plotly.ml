0b8bea08f7274ee32eb3351d03031016a3b43a73
John Hester <hesterj@etableau.com>
2025-01-01 09:57:20 -0600

    feat: new Plotly.of_bars

--- src/plotly.ml
+++ src/plotly.ml
@@ -115,70 +115,86 @@ let order_trace (side : Trading_types.Side.t) (data : Item.t list) (symbol : str
       ("name", `String (Trading_types.Side.to_string side));
     ]
 
-let of_bars (x : Bars.t) (indicators : Indicators.t) (symbol : string) :
-    Yojson.Safe.t option =
-  let* data_vec = Bars.get x symbol in
-  let+ ema_trace = ema_trace indicators symbol in
+let of_bars bars indicators symbol  : Yojson.Safe.t option =
+  let* data_vec = Bars.get bars symbol in
   let data = Vector.to_list data_vec in
-  let x_axis =
-    let mk_plotly_x x =
-      let time = Item.timestamp x in
-      let res = Ptime.to_rfc3339 time in
-      `String res
-    in
-    List.map mk_plotly_x data
-  in
-  let buy_axis =
-    List.map
-      (fun (x : Item.t) ->
-        match Item.order x with
-        | None -> `Null
-        | Some order -> (
-            match order.side with Buy -> `String "buy" | Sell -> `Null))
-      data
-  in
-  let reasons =
-    List.map
-      (fun (x : Item.t) ->
-        Item.order x |> function
-        | None -> `Null
-        | Some order -> `List (List.map (fun x -> `String x) order.reason))
-      data
-  in
-  let sell_axis =
-    List.map
-      (fun (x : Item.t) ->
-        match Item.order x with
-        | None -> `Null
-        | Some order -> (
-            match order.side with Buy -> `Null | Sell -> `String "sell"))
-      data
-  in
-  let y_axis = List.map (fun x -> `Float (Item.last x)) data in
-  `Assoc
-    [
-      ( "data",
-        `List
-          [
-            `Assoc
-              [
-                ("x", `List x_axis);
-                ("y", `List y_axis);
-                ("buy", `List buy_axis);
-                ("sell", `List sell_axis);
-                ("reasons", `List reasons);
-                ("ema", ema_trace);
-                ("mode", `String "lines+markers");
-                ("name", `String symbol);
-              ];
-          ] );
-      ( "layout",
-        `Assoc
-          [
-            ("title", `String "Sample Plotly Graph");
-            ( "xaxis",
-              `Assoc [ ("title", `String "X Axis"); ("type", `String "date") ]
-            );
-            ("yaxis", `Assoc [ ("title", `String "Y Axis") ]);
-          ] );
-    ]
+  let+ ema_trace = ema_trace indicators symbol in
+  let buy_trace = order_trace Buy data symbol in
+  let sell_trace = order_trace Sell data symbol in
+  let price_trace =  price_trace data symbol in
+  let ( = ) = fun x y -> (x, y) in
+  `Assoc [
+    "price_trace" = price_trace;
+    "buy_trace" = buy_trace;
+    "sell_trace" = sell_trace;
+    "ema_trace" = ema_trace;
+    "layout" = layout symbol;
+  ]
+
+(* let of_bars (x : Bars.t) (indicators : Indicators.t) (symbol : string) : *)
+(*     Yojson.Safe.t option = *)
+(*   let* data_vec = Bars.get x symbol in *)
+(*   let+ ema_trace = ema_trace indicators symbol in *)
+(*   let data = Vector.to_list data_vec in *)
+(*   let x_axis = *)
+(*     let mk_plotly_x x = *)
+(*       let time = Item.timestamp x in *)
+(*       let res = Ptime.to_rfc3339 time in *)
+(*       `String res *)
+(*     in *)
+(*     List.map mk_plotly_x data *)
+(*   in *)
+(*   let buy_axis = *)
+(*     List.map *)
+(*       (fun (x : Item.t) -> *)
+(*         match Item.order x with *)
+(*         | None -> `Null *)
+(*         | Some order -> ( *)
+(*             match order.side with Buy -> `String "buy" | Sell -> `Null)) *)
+(*       data *)
+(*   in *)
+(*   let reasons = *)
+(*     List.map *)
+(*       (fun (x : Item.t) -> *)
+(*         Item.order x |> function *)
+(*         | None -> `Null *)
+(*         | Some order -> `List (List.map (fun x -> `String x) order.reason)) *)
+(*       data *)
+(*   in *)
+(*   let sell_axis = *)
+(*     List.map *)
+(*       (fun (x : Item.t) -> *)
+(*         match Item.order x with *)
+(*         | None -> `Null *)
+(*         | Some order -> ( *)
+(*             match order.side with Buy -> `Null | Sell -> `String "sell")) *)
+(*       data *)
+(*   in *)
+(*   let y_axis = List.map (fun x -> `Float (Item.last x)) data in *)
+(*   `Assoc *)
+(*     [ *)
+(*       ( "data", *)
+(*         `List *)
+(*           [ *)
+(*             `Assoc *)
+(*               [ *)
+(*                 ("x", `List x_axis); *)
+(*                 ("y", `List y_axis); *)
+(*                 ("buy", `List buy_axis); *)
+(*                 ("sell", `List sell_axis); *)
+(*                 ("reasons", `List reasons); *)
+(*                 ("ema", ema_trace); *)
+(*                 ("mode", `String "lines+markers"); *)
+(*                 ("name", `String symbol); *)
+(*               ]; *)
+(*           ] ); *)
+(*       ( "layout", *)
+(*         `Assoc *)
+(*           [ *)
+(*             ("title", `String "Sample Plotly Graph"); *)
+(*             ( "xaxis", *)
+(*               `Assoc [ ("title", `String "X Axis"); ("type", `String "date") ] *)
+(*             ); *)
+(*             ("yaxis", `Assoc [ ("title", `String "Y Axis") ]); *)
+(*           ] ); *)
+(*     ] *)
