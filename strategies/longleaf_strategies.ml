a9d87e2ec3c9204dc6cc4ea0349ba1bb9dba9f99
John Hester <hesterj@etableau.com>
2025-01-09 19:00:49 -0600

    feat: simplify backend/strat instantiation

--- strategies/longleaf_strategies.ml
+++ strategies/longleaf_strategies.ml
@@ -1,106 +1,7 @@
-module Run_options = struct
-  type t = {
-    symbols : string list;
-    tick : float;
-    overnight : bool;
-    resume_after_liquidate : bool;
-  }
-end
-
-module Run_context = struct
-  type t = {
-    eio_env : Eio_unix.Stdenv.base;
-    longleaf_env : Environment.t;
-    switch : Eio.Switch.t;
-    preload : Options.Preload.t;
-    target : string option;
-    save_received : bool;
-    mutices : Longleaf_mutex.t;
-    run_options : Run_options.t option;
-  }
-end
-
-module type RUN_CONTEXT = sig end
-
-module type S = sig
-  val top : Options.Runtype.t -> unit
-end
-
-module Make (StrategyBuilder : Strategy.BUILDER) (Context : RUN_CONTEXT) : S =
-struct
-  module Input : Backend.BACKEND_INPUT = struct
-    include Context
-
-    let run_options =
-      Option.get_exn_or
-        "Must have run options to instantiate strategy and backend"
-        Context.run_options
-
-    let symbols = run_options.symbols
-    let overnight = run_options.overnight
-    let resume_after_liquidate = run_options.resume_after_liquidate
-    let tick = run_options.tick
-
-    (* Target *)
-    let target =
-      let ( let+ ) = Option.( let+ ) in
-      let+ res =
-        Context.target
-        |> Option.map @@ fun f -> Yojson.Safe.from_file f |> Bars.t_of_yojson
-      in
-      Bars.sort (Ord.opp Item.compare) res;
-      res
-
-    (* Preload *)
-    let bars =
-      match Context.preload with
-      | None -> Bars.empty ()
-      | Download -> invalid_arg "Downloading data for preload NYI"
-      | File file ->
-          Eio.traceln "Preloading bars from %s" file;
-          let res = Yojson.Safe.from_file file |> Bars.t_of_yojson in
-          Bars.sort Item.compare res;
-          res
-  end
-
-  module Backtesting : Backend.S = Backend.Backtesting (Input)
-  module Alpaca : Backend.S = Backend.Alpaca (Input)
-
-  let live () = invalid_arg "Live trading is not implemented yet"
-  let manual () = invalid_arg "Cannot create a strategy with manual runtype"
-
-  let paper () =
-    Eio.traceln "@[Creating Alpaca backend with:@.tick: %f@.symbols: %a@]@."
-      Input.tick
-      List.(pp String.pp)
-      Input.symbols;
-    let module Strategy = StrategyBuilder (Alpaca) in
-    Strategy.run ()
-
-  let listener = paper
-
-  let backtest () =
-    let module Strategy = StrategyBuilder (Backtesting) in
-    Strategy.run ()
-
-  let top (runtype : Options.Runtype.t) =
-    let res =
-      match runtype with
-      | Live -> live ()
-      | Manual -> manual ()
-      | Paper -> paper ()
-      | Listener -> listener ()
-      | BuyAndHold -> backtest ()
-      | Backtest -> backtest ()
-    in
-    Alpaca.shutdown ();
-    Backtesting.shutdown ();
-    Eio.traceln "Result: %s" res;
-    ()
-end
+module Run_options = Backend.Run_options
 
 module DoubleTop = struct
-  let run_options : Run_options.t =
+  let run_options runtype : Run_options.t =
     {
       symbols =
         [
@@ -127,13 +28,20 @@ module DoubleTop = struct
       tick = 600.0;
       overnight = false;
       resume_after_liquidate = true;
+      runtype;
     }
 
-  module Make = Make (Data) (Double_top.DoubleTop)
+  let top runtype context =
+    let options = run_options runtype in
+    let module Backend = (val Backend.create_backend options context) in
+    let module S = Double_top.DoubleTop (Backend) in
+    let res = S.run () in
+    Backend.shutdown ();
+    res
 end
 
 module LowBall = struct
-  let run_options : Run_options.t =
+  let run_options runtype : Run_options.t =
     {
       symbols =
         [
@@ -159,13 +67,20 @@ module LowBall = struct
       tick = 600.0;
       overnight = true;
       resume_after_liquidate = true;
+      runtype;
     }
 
-  module Make = Make (Data) (Buy_low_bollinger.BuyLowBollinger)
+  let top runtype context =
+    let options = run_options runtype in
+    let module Backend = (val Backend.create_backend options context) in
+    let module S = Buy_low_bollinger.BuyLowBollinger (Backend) in
+    let res = S.run () in
+    Backend.shutdown ();
+    res
 end
 
 module Listener = struct
-  let run_options : Run_options.t =
+  let run_options runtype : Run_options.t =
     {
       symbols =
         [
@@ -191,19 +106,33 @@ module Listener = struct
       tick = 600.0;
       overnight = true;
       resume_after_liquidate = true;
+      runtype;
     }
 
-  module Make = Make (Data) (Listener.Make)
+  let top runtype context =
+    let options = run_options runtype in
+    let module Backend = (val Backend.create_backend options context) in
+    let module S = Listener.Make (Backend) in
+    let res = S.run () in
+    Backend.shutdown ();
+    res
 end
 
 module BuyAndHold = struct
-  let run_options : Run_options.t =
+  let run_options runtype : Run_options.t =
     {
       symbols = [ "SPY" ];
       tick = 600.0;
       overnight = true;
       resume_after_liquidate = true;
+      runtype;
     }
 
-  module Make = Make (Data) (Buy_and_hold.Make)
+  let top runtype context =
+    let options = run_options runtype in
+    let module Backend = (val Backend.create_backend options context) in
+    let module S = Buy_and_hold.Make (Backend) in
+    let res = S.run () in
+    Backend.shutdown ();
+    res
 end
