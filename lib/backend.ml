e124b7cd9121e348e2f5accb2af4bfbdcc6ca404
John Hester <hesterj@etableau.com>
2025-01-21 16:39:53 -0600

    feat: randomized backtest length option

--- lib/backend.ml
+++ lib/backend.ml
@@ -122,6 +122,7 @@ module Run_options = struct
     runtype : Options.Runtype.t;
     indicators_config : Indicators.Config.t;
     dropout : bool;
+    randomized_backtest_length : int;
   }
 end
 
