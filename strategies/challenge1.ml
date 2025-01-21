dff6468e1098469bac5a4c9cd2cf16af97c4cbcf
John Hester <hesterj@etableau.com>
2025-01-20 20:20:06 -0600

    feat: challenge1 can hold multiple positions

--- strategies/challenge1.ml
+++ strategies/challenge1.ml
@@ -1,7 +1,7 @@
 open Trading_types
 
 module Status = struct
-  type t = Placed of (int * Order.t) | Waiting [@@deriving show]
+  type t = Order.t list [@@deriving show]
 end
 
 type state = Status.t State.t
@@ -13,17 +13,28 @@ module BuyReason = struct
   let make (state : state) symbol =
     let open Option.Infix in
     let current_price = Bars.Latest.get state.latest symbol |> Item.last in
+    let is_owned =
+      state.content
+      |> List.map (fun (x : Order.t) -> x.symbol)
+      |> List.mem symbol
+    in
     let* upper_bb =
-      Indicators.indicator state.indicators symbol
-        Indicators.Point.upper_bollinger_100_3
+      Indicators.get_indicator state.indicators symbol
+        (* Indicators.Point.upper_bollinger_100_3 *)
+        (* Indicators.Point.upper_bollinger_100_1 *)
+        Indicators.Point.upper_bollinger
     in
     let sma75spy =
-      Indicators.indicator state.indicators "SPY" Indicators.Point.sma_75
+      Indicators.get_indicator state.indicators "SPY" Indicators.Point.sma_75
       |> Option.get_exn_or "Must be able to get SPY SMA 75"
     in
     let spy_price = Bars.Latest.get state.latest "SPY" |> Item.last in
     let amt_above = current_price -. upper_bb in
-    let pass = current_price >=. upper_bb && not (spy_price <=. sma75spy) in
+    let pass =
+      current_price >=. upper_bb
+      && (not (spy_price <=. sma75spy))
+      && not is_owned
+    in
     match pass with true -> Some { symbol; amt_above } | false -> None
 end
 
@@ -35,11 +46,12 @@ module SellReason = struct
     let open Option.Infix in
     let current_price = Bars.Latest.get state.latest symbol |> Item.last in
     let* lower_bb =
-      Indicators.indicator state.indicators symbol
+      Indicators.get_indicator state.indicators symbol
         Indicators.Point.lower_bollinger_100_1
+      (* Indicators.Point.lower_bollinger *)
     in
     let* sma75spy =
-      Indicators.indicator state.indicators "SPY" Indicators.Point.sma_75
+      Indicators.get_indicator state.indicators "SPY" Indicators.Point.sma_75
     in
     let spy_price = Bars.Latest.get state.latest "SPY" |> Item.last in
     let pass = current_price <=. lower_bb && not (spy_price <=. sma75spy) in
@@ -53,6 +65,7 @@ module Make (Backend : Backend.S) : Strategy.S = struct
     include Trading_types.Order
 
     let of_buy_reason (state : state) (x : BuyReason.t) =
+      let ( let+ ) = Option.( let+ ) in
       match x with
       | { symbol; _ } ->
           let current_cash = Backend.get_cash () in
@@ -61,12 +74,16 @@ module Make (Backend : Backend.S) : Strategy.S = struct
           let tif = TimeInForce.GoodTillCanceled in
           let order_type = OrderType.Market in
           let price = Item.last item in
-          let qty = Math.qty ~current_cash ~pct:1.0 ~price in
+          let+ qty =
+            match Math.qty ~current_cash ~pct:1.0 ~price with
+            | 0 -> None
+            | qty -> Some qty
+          in
           let timestamp = Item.timestamp item in
           let reason =
             [
-              Format.asprintf "Buying (%a): Above 3 std bollinger band" Time.pp
-                timestamp;
+              Format.asprintf "Buying %d (%a): Above 3 std bollinger band" qty
+                Time.pp timestamp;
             ]
           in
           Order.make ~symbol ~side ~tif ~order_type ~qty ~price ~timestamp
@@ -88,8 +105,8 @@ module Make (Backend : Backend.S) : Strategy.S = struct
           in
           let reason =
             [
-              Format.asprintf "Selling (%a): Below 1 std bollinger band" Time.pp
-                timestamp;
+              Format.asprintf "Selling %d (%a): Below 1 std bollinger band" qty
+                Time.pp timestamp;
             ]
           in
           Order.make ~symbol ~side ~tif ~order_type ~qty ~price ~timestamp
@@ -100,7 +117,7 @@ module Make (Backend : Backend.S) : Strategy.S = struct
     Eio.traceln "Shutdown command NYI";
     ()
 
-  let init_state = Backend.init_state Status.Waiting
+  let init_state = Backend.init_state []
 
   let buy (state : state) =
     let open Result.Infix in
@@ -115,45 +132,57 @@ module Make (Backend : Backend.S) : Strategy.S = struct
               if previous.amt_above <=. x.amt_above then Some previous
               else Some x)
         None passes
+      |> fun selection -> Option.bind selection (Order.of_buy_reason state)
     in
     let+ content =
       match selected with
       | None -> Ok state.content
       | Some choice ->
-          let order = Order.of_buy_reason state choice in
-          let* () = Backend.place_order state order in
-          Result.return @@ Status.Placed (0, order)
+          let* () = Backend.place_order state choice in
+          Result.return @@ (choice :: state.content)
     in
     { state with State.current = `Listening; content }
 
-  let sell (state : state) ~(buying_order : Order.t) ~time_held =
+  let sell (state : state) ~(buying_order : Order.t) =
     let open Result.Infix in
     match SellReason.make state buying_order.symbol with
     | None ->
         Result.return
-        @@ {
-             state with
-             State.current = `Listening;
-             content = Status.Placed (time_held + 1, buying_order);
-           }
+        @@ { state with State.current = `Listening; content = [ buying_order ] }
     | Some reason ->
         let order = Order.of_sell_reason ~buying_order state reason in
         let* () = Backend.place_order state order in
+        let new_content =
+          List.filter (fun x -> not @@ Order.equal x buying_order) state.content
+        in
         Result.return
-        @@ { state with State.current = `Listening; content = Status.Waiting }
+        @@ { state with State.current = `Listening; content = new_content }
+
+  let sell_fold state buying_order =
+    let ( let* ) = Result.( let* ) in
+    let* state = state in
+    let* res = sell state ~buying_order in
+    Result.return res
 
   let step (state : state) =
+    let ( let* ) = Result.( let* ) in
     let current = state.current in
     (* Eio.traceln "@[buylowbollinger: %a@]@." State.pp_state current; *)
     match current with
     | #State.nonlogical_state as current ->
         SU.handle_nonlogical_state current state
     | `Ordering -> (
-        (* Eio.traceln "@[%a@]@." DT_Status.pp state.content; *)
-        match state.content with
-        | Waiting -> buy state
-        | Placed (time_held, buying_order) ->
-            sell ~buying_order ~time_held state)
+        let positions = state.content in
+        let length = List.length positions in
+        (* Eio.traceln "%d positions" length; *)
+        match length with
+        | 0 ->
+            let* sold_high =
+              List.fold_left sell_fold (Ok state) state.content
+            in
+            let* purchase = buy sold_high in
+            Result.return purchase
+        | _ -> List.fold_left sell_fold (Ok state) state.content)
 
   let run () = SU.run ~init_state step
 end
