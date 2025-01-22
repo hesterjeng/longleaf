c866009696b97c438de70693123ed7bcff63729c
John Hester <hesterj@etableau.com>
2025-01-22 17:54:03 -0600

    feat: add an interface for backend

---
+++ lib/backend.mli
@@ -0,0 +1,8 @@
+module type S = Backend_intf.S
+
+val make :
+  Backend_intf.Run_options.t ->
+  Backend_intf.Run_context.t ->
+  (module Backend_intf.S)
+(** Using only the options and context, create an instantiated backend. This
+    backend can then be used to instantiate a strategy for running.*)
