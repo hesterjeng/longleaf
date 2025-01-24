8153c5d4f4214595008708030e1d6d347ccb8f5a
John Hester <hesterj@etableau.com>
2025-01-23 18:06:59 -0600

    chore: rename file

--- lib/signals.ml
+++ lib/signals.ml
@@ -5,23 +5,21 @@ end
 type res = Pass of float | Fail of float
 type 'a t = 'a State.t -> string -> res option
 
-module Examples = struct
-  let small_rsi : 'a t =
-   fun state symbol ->
-    let open Option in
-    let* indicators = Indicators.get state.indicators symbol in
-    let* point = Vector.top indicators in
-    let rsi = Indicators.Point.rsi point in
-    if rsi <=. 40.0 then Some (Pass rsi) else None
+let rsi : 'a t =
+ fun state symbol ->
+  let open Option in
+  let* indicators = Indicators.get state.indicators symbol in
+  let* point = Vector.top indicators in
+  let rsi = Indicators.Point.rsi point in
+  if rsi <=. 40.0 then Some (Pass rsi) else None
 
-  let above_awesome : 'a t =
-   fun state symbol ->
-    let open Option in
-    let* indicators = Indicators.get state.indicators symbol in
-    let* point = Vector.top indicators in
-    let awesome = Indicators.Point.awesome point in
-    if Indicators.Point.awesome point >=. 1.0 then Some (Fail awesome) else None
-end
+let awesome : 'a t =
+ fun state symbol ->
+  let open Option in
+  let* indicators = Indicators.get state.indicators symbol in
+  let* point = Vector.top indicators in
+  let awesome = Indicators.Point.awesome point in
+  if Indicators.Point.awesome point >=. 1.0 then Some (Fail awesome) else None
 
 let bind o f =
   match o with Some (Pass res) -> f res | Some (Fail _) -> None | None -> None
