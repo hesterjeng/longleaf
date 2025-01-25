c50872bf2d4ef71b68416406a9d3dcb9d4f541ab
John Hester <hesterj@etableau.com>
2025-01-24 18:41:34 -0600

    chore: add more comments

--- strategies/template_example.ml
+++ strategies/template_example.ml
@@ -1,3 +1,8 @@
+(* This is an example of how to use the templates to easily make strategies. *)
+(* Once the Make functor is instantiated with a backend, the strategy is ready to run. *)
+(* Add a hook for it in longleaf_strategies.ml *)
+
+(* We need a module to see what symbols pass our buy filter, and a way to score the passes *)
 module Buy_inp : Template.Buy_trigger.INPUT = struct
   let pass (state : 'a State.t) symbol =
     let price = Signal.Indicator.price state symbol in
@@ -15,8 +20,10 @@ module Buy_inp : Template.Buy_trigger.INPUT = struct
     price -. upper_bb
 end
 
+(* The functor uses the score to choose the symbol with the highest score *)
 module Buy = Template.Buy_trigger.Make (Buy_inp)
 
+(* We will sell any symbol that meets the requirement *)
 module Sell : Template.Sell_trigger.S = struct
   let make (state : 'a State.t) symbol =
     let price = Signal.Indicator.price state symbol in
@@ -24,4 +31,5 @@ module Sell : Template.Sell_trigger.S = struct
     @@ [ Signal.Indicator.lower_bb symbol Above price ]
 end
 
+(* Create a strategy with our parameters *)
 module Make : Strategy.BUILDER = Template.Make (Buy) (Sell)
