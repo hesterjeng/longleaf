c21a2f56fddc3e0fc27d1f133ab3b59de74cf131
John Hester <hesterj@etableau.com>
2025-01-26 14:56:17 -0600

    chore: format

--- strategies/lowboll2.ml
+++ strategies/lowboll2.ml
@@ -8,37 +8,27 @@ module F = S.Flag
 module Buy_inp : Template.Buy_trigger.INPUT = struct
   let pass (state : 'a State.t) symbol =
     let price = State.price state symbol in
+    let i = Indicators.get_top state.indicators symbol in
     let conditions =
       [
-        ( Indicators.get_indicator state.indicators symbol P.sma_5
-        |> fun sma_5 ->
-          match price /. sma_5 >=. 0.95 with
-          | true -> F.Pass [ "Above SMA(5) (pass crash block)" ]
-          | false -> Fail [ "Below SMA(5) (crash block)" ] );
-        ( Indicators.get_indicator state.indicators symbol P.lower_bollinger
-        |> fun lower_bb ->
-          match price <=. lower_bb with
-          | true -> F.Pass [ "Below Lower BB" ]
-          | false -> F.Fail [ "Not below Lower BB" ] );
-        ( Indicators.get_indicator state.indicators symbol P.rsi |> fun rsi ->
-          match rsi <=. 40.0 with
-          | true -> F.Pass [ "Small RSI" ]
-          | false -> Fail [ "RSI too large to buy" ] );
-        ( Indicators.get_indicator state.indicators symbol P.awesome
-        |> fun awesome ->
-          match awesome >=. 1.0 with
-          | true -> F.Pass [ "Awesome above 1.0" ]
-          | false -> Fail [ "Awesome too low to buy" ] );
-        ( Indicators.get_indicator state.indicators symbol P.awesome_slow
-        |> fun awesome_slow ->
-          match awesome_slow >=. 1.0 with
-          | true -> F.Pass [ "Awesome slow above 1.0" ]
-          | false -> Fail [ "Awesome slow too low to buy" ] );
-        ( Indicators.get_indicator state.indicators symbol P.fso_pk
-        |> fun fso_pk ->
-          match fso_pk >=. 50.0 with
-          | true -> F.Fail [ "FSO %K too high" ]
-          | false -> F.Pass [ "FSO %K is low enough" ] );
+        (match price /. i.sma_5 >=. 0.95 with
+        | true -> F.Pass [ "Above SMA(5) (pass crash block)" ]
+        | false -> Fail [ "Below SMA(5) (crash block)" ]);
+        (match price <=. i.lower_bollinger with
+        | true -> F.Pass [ "Below Lower BB" ]
+        | false -> F.Fail [ "Not below Lower BB" ]);
+        (match i.relative_strength_index <=. 40.0 with
+        | true -> F.Pass [ "Small RSI" ]
+        | false -> Fail [ "RSI too large to buy" ]);
+        (match i.awesome_oscillator >=. 1.0 with
+        | true -> F.Pass [ "Awesome above 1.0" ]
+        | false -> Fail [ "Awesome too low to buy" ]);
+        (match i.awesome_slow >=. 1.0 with
+        | true -> F.Pass [ "Awesome slow above 1.0" ]
+        | false -> Fail [ "Awesome slow too low to buy" ]);
+        (match i.fast_stochastic_oscillator_k >=. 50.0 with
+        | true -> F.Fail [ "FSO %K too high" ]
+        | false -> F.Pass [ "FSO %K is low enough" ]);
       ]
     in
     List.fold_left F.and_fold (Pass []) conditions
@@ -48,7 +38,7 @@ module Buy_inp : Template.Buy_trigger.INPUT = struct
     let lower_bb = I.get_indicator state.indicators symbol P.lower_bollinger in
     lower_bb /. price
 
-  let num_positions = 5
+  let num_positions = 1
 end
 
 (* The functor uses the score to choose the symbol with the highest score *)
