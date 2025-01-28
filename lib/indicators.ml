e743b2ba4b3c2b58f2593e3418dd6516bc803521
John Hester <hesterj@etableau.com>
2025-01-28 03:32:14 -0600

    feat: some optimization, more messages

--- lib/indicators.ml
+++ lib/indicators.ml
@@ -374,27 +374,27 @@ let initialize config bars symbol =
     | None ->
         invalid_arg "Expected to have bars data when initializing indicators"
   in
-  let bars = Vector.to_list bars_vec in
+  (* let bars = Vector.to_list bars_vec in *)
+  let bars_upto_now = Vector.create () in
   let _ =
-    List.foldi
-      (fun previous i item ->
+    Vector.foldi
+      (fun i previous item ->
         let timestamp = Item.timestamp item in
         match previous with
         | None ->
             let res = Point.initial timestamp in
             Vector.push initial_stats_vector res;
+            Vector.push bars_upto_now (Vector.get bars_vec i);
             Option.return res
         | Some previous ->
-            let bars_vec_upto_now =
-              Vector.slice_iter bars_vec 0 i |> Vector.of_iter
-            in
+            Vector.push bars_upto_now (Vector.get bars_vec i);
             let res =
-              Point.of_latest config timestamp bars_vec_upto_now previous
+              Point.of_latest config timestamp bars_upto_now previous
                 initial_stats_vector item
             in
             Vector.push initial_stats_vector res;
             Option.return res)
-      None bars
+      None bars_vec
   in
   initial_stats_vector
 
@@ -414,7 +414,7 @@ let add_latest config timestamp (bars : Bars.t) (latest_bars : Bars.Latest.t)
     match get x symbol with
     | Some i -> i
     | None ->
-        (* Eio.traceln "Creating initial indicators for %s." symbol; *)
+        Eio.traceln "Creating initial indicators for %s..." symbol;
         let new_vector = initialize config bars symbol in
         Hashtbl.replace x symbol new_vector;
         new_vector
