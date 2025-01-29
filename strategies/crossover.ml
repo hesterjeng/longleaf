60f579fb68e6043b6907bf411418bf2b9df26115
John Hester <hesterj@etableau.com>
2025-01-29 02:47:37 -0600

    feat: Flag.let*, fix fsopk default value, crossover

--- strategies/crossover.ml
+++ strategies/crossover.ml
@@ -5,37 +5,36 @@ module P = I.Point
 module F = S.Flag
 
 module Param = struct
-  let stop_loss_multiplier = 0.5
+  let stop_loss_multiplier = 0.98
 
   (* let profit_multiplier = 1.04 *)
-  let max_holding_period = 600
+  let max_holding_period = 6
 end
+
+let ( let* ) = F.Infix.( let* )
 
 (* We need a module to see what symbols pass our buy filter, and a way to score the passes *)
 module Buy_inp : Template.Buy_trigger.INPUT = struct
   let pass (state : 'a State.t) symbol =
     let price = State.price state symbol in
     let i = Indicators.get_top state.indicators symbol in
+    let* prev = i.previous in
     let conditions =
       [
-        (* (match price /. i.sma_5 >=. 0.95 with *)
-        (* | true -> F.Pass [ "Above SMA(5) (pass crash block)" ] *)
-        (* | false -> Fail [ "Below SMA(5) (crash block)" ]); *)
-        (match price <=. i.lower_bollinger with
-        | true -> F.Pass [ "Below Lower BB" ]
-        | false -> F.Fail [ "Not below Lower BB" ]);
-        (* (match i.relative_strength_index <=. 40.0 with *)
-        (* | true -> F.Pass [ "Small RSI" ] *)
-        (* | false -> Fail [ "RSI too large to buy" ]); *)
-        (* (match i.awesome_oscillator >=. 1.0 with *)
-        (* | true -> F.Pass [ "Awesome above 1.0" ] *)
-        (* | false -> Fail [ "Awesome too low to buy" ]); *)
-        (* (match i.awesome_slow >=. 1.0 with *)
-        (* | true -> F.Pass [ "Awesome slow above 1.0" ] *)
-        (* | false -> Fail [ "Awesome slow too low to buy" ]); *)
-        (* (match i.fast_stochastic_oscillator_k >=. 50.0 with *)
-        (* | true -> F.Fail [ "FSO %K too high" ] *)
-        (* | false -> F.Pass [ "FSO %K is low enough" ]); *)
+        (match i.relative_strength_index <=. 40.0 with
+        | true -> F.Pass [ "Small RSI" ]
+        | false -> Fail [ "RSI too large to buy" ]);
+        (match i.fast_stochastic_oscillator_d <=. 20.0 with
+        | true -> F.Pass [ "FSO %D <= 20" ]
+        | false -> F.Fail [ "FSO %D is too high" ]);
+        (let crossover =
+           prev.fast_stochastic_oscillator_k
+           <=. prev.fast_stochastic_oscillator_d
+           && i.fast_stochastic_oscillator_k >=. i.fast_stochastic_oscillator_d
+         in
+         match crossover with
+         | true -> F.Pass [ "Bullish Crossover" ]
+         | false -> F.Fail [ "No Crossover" ]);
       ]
     in
     List.fold_left F.and_fold (Pass []) conditions
@@ -45,7 +44,7 @@ module Buy_inp : Template.Buy_trigger.INPUT = struct
     let lower_bb = I.get_indicator state.indicators symbol P.lower_bollinger in
     lower_bb /. price
 
-  let num_positions = 1
+  let num_positions = 5
 end
 
 (* The functor uses the score to choose the symbol with the highest score *)
@@ -56,20 +55,24 @@ module Sell : Template.Sell_trigger.S = struct
   let make (state : 'a State.t) ~(buying_order : Order.t) =
     let price = State.price state buying_order.symbol in
     let i = Indicators.get_top state.indicators buying_order.symbol in
+    let* prev = i.previous in
     let conditions =
       [
-        (match
-           i.fast_stochastic_oscillator_k >=. 90.0
-           && price >. buying_order.price
-         with
-        | true -> F.Pass [ "High FSO %k!" ]
-        | false -> F.Fail [ "FSO %D too low to sell" ]);
-        (match state.tick >= buying_order.tick + Param.max_holding_period with
-        | true -> F.Pass [ "Holding period exceeded" ]
-        | false -> F.Fail [ "Holding period OK" ]);
+        (* (let crossover = *)
+        (*    prev.fast_stochastic_oscillator_k *)
+        (*    >=. prev.fast_stochastic_oscillator_d *)
+        (*    && i.fast_stochastic_oscillator_k <=. i.fast_stochastic_oscillator_d *)
+        (*  in *)
+        (*  let overbought = i.fast_stochastic_oscillator_d >=. 80.0 in *)
+        (*  match overbought && crossover with *)
+        (*  | true -> F.Pass [ "Bearish Crossover" ] *)
+        (*  | false -> F.Fail [ "No Crossover" ]); *)
         (* (match price <=. Param.stop_loss_multiplier *. buying_order.price with *)
         (* | true -> F.Pass [ "Stop loss triggered" ] *)
         (* | false -> F.Fail [ "Stop loss not triggered" ]); *)
+        (match state.tick >= buying_order.tick + Param.max_holding_period with
+        | true -> F.Pass [ "Holding period exceeded" ]
+        | false -> F.Fail [ "Holding period OK" ]);
       ]
     in
     List.fold_left F.or_fold (Fail []) conditions
