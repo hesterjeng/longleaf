d4067f5cdd10ce1659369159c3ea76d7c680237d
John Hester <hesterj@etableau.com>
2025-01-29 03:34:30 -0600

    feat: basic tearsheet

--- lib/tearsheet.ml
+++ lib/tearsheet.ml
@@ -1,9 +1,18 @@
-type t = { num_orders : int; sharpe_ratio : float; win_loss_ratio : float }
+type t = {
+  num_orders : int;
+  sharpe_ratio : float;
+  win_loss_ratio : float;
+  average_trade_net : float;
+  average_profit : float;
+  average_loss : float;
+  stddev_returns : float;
+  profit_factor : float;
+}
 [@@deriving show]
 
 let num_orders (x : Order_history.t) = Vector.length x
 
-let win_loss (h : Order_history.t) =
+let win_loss_ratio (h : Order_history.t) =
   let fold (winners, losers) (x : Order.t) =
     match x.profit with
     | None -> (winners, losers)
@@ -30,7 +39,7 @@ let sharpe_ratio (stats : Stats.t) =
   let sharpe = (final.value -. final.risk_free_value) /. std in
   sharpe
 
-let average_gain_loss (h : Order_history.t) =
+let average_trade_net (h : Order_history.t) =
   let ( let+ ) = Option.( let+ ) in
   let nets =
     Vector.filter_map
@@ -41,3 +50,66 @@ let average_gain_loss (h : Order_history.t) =
     |> Vector.to_array
   in
   Owl_stats.mean nets
+
+let average_profit (h : Order_history.t) =
+  let ( let* ) = Option.( let* ) in
+  let nets =
+    Vector.filter_map
+      (fun (x : Order.t) ->
+        let* profit = x.profit in
+        match profit >=. 0.0 with true -> Some profit | false -> None)
+      h
+    |> Vector.to_array
+  in
+  Owl_stats.mean nets
+
+let average_loss (h : Order_history.t) =
+  let ( let* ) = Option.( let* ) in
+  let nets =
+    Vector.filter_map
+      (fun (x : Order.t) ->
+        let* profit = x.profit in
+        match profit <=. 0.0 with true -> Some profit | false -> None)
+      h
+    |> Vector.to_array
+  in
+  Owl_stats.mean nets
+
+let stddev_returns (stats : Stats.t) =
+  let returns =
+    List.map (fun (x : Stats.item) -> x.value) stats |> Array.of_list
+  in
+  Owl_stats.std returns
+
+let profit_factor (h : Order_history.t) =
+  let profits =
+    Vector.fold
+      (fun acc (x : Order.t) ->
+        match x.profit with
+        | None -> acc
+        | Some f when f >=. 0.0 -> acc +. f
+        | _ -> acc)
+      0.0 h
+  in
+  let losses =
+    Vector.fold
+      (fun acc (x : Order.t) ->
+        match x.profit with
+        | None -> acc
+        | Some f when f <=. 0.0 -> acc +. f
+        | _ -> acc)
+      0.0 h
+  in
+  profits /. losses
+
+let make (h : Order_history.t) (stats : Stats.t) : t =
+  {
+    num_orders = num_orders h;
+    sharpe_ratio = sharpe_ratio stats;
+    win_loss_ratio = win_loss_ratio h;
+    average_trade_net = average_trade_net h;
+    average_profit = average_profit h;
+    average_loss = average_loss h;
+    profit_factor = profit_factor h;
+    stddev_returns = stddev_returns stats;
+  }
