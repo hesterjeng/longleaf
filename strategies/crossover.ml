9794193ffe19d6f77b284819cc72af9f2f5b0d7d
John Hester <hesterj@etableau.com>
2025-01-30 19:21:59 -0600

    play: crossover

--- strategies/crossover.ml
+++ strategies/crossover.ml
@@ -8,7 +8,7 @@ module Param = struct
   let stop_loss_multiplier = 0.98
 
   (* let profit_multiplier = 1.03 *)
-  let max_holding_period = 6
+  let max_holding_period = 600
 end
 
 let ( let* ) = F.Infix.( let* )
@@ -50,7 +50,7 @@ module Buy_inp : Template.Buy_trigger.INPUT = struct
   (* let lower_bb = I.get_indicator state.indicators symbol P.lower_bollinger in *)
   (* lower_bb /. price *)
 
-  let num_positions = 1
+  let num_positions = 10
 end
 
 (* The functor uses the score to choose the symbol with the highest score *)
@@ -59,29 +59,41 @@ module Buy = Template.Buy_trigger.Make (Buy_inp)
 (* We will sell any symbol that meets the requirement *)
 module Sell : Template.Sell_trigger.S = struct
   let make (state : 'a State.t) ~(buying_order : Order.t) =
-    (* let price = State.price state buying_order.symbol in *)
-    (* let i = Indicators.get_top state.indicators buying_order.symbol in *)
+    let price = State.price state buying_order.symbol in
+    let i = Indicators.get_top state.indicators buying_order.symbol in
     (* let* prev = i.previous in *)
+    (* This strategy also seems to do OK if we sell no matter what after 6 ticks *)
+    (* let conditions = *)
+    (*   [ *)
+    (*     (match state.tick >= buying_order.tick + Param.max_holding_period with *)
+    (*     | true -> *)
+    (*       ( *)
+    (*         match price >=. buying_order.price with *)
+    (*         | false -> F.Pass [ "Price hasn't increased, exit" ] *)
+    (*         | true -> *)
+    (*           ( *)
+    (*             match i.fast_stochastic_oscillator_d >=. 20.0 with *)
+    (*             | true -> F.Pass [ "FSO %D greater than 20" ] *)
+    (*             | false -> F.Fail [ "FSO %K not high enough" ] *)
+    (*           ) *)
+    (*       ) *)
+    (*       (\* F.Pass [ "Holding period exceeded" ] *\) *)
+    (*     | false -> F.Fail [ "Holding period OK" ]); *)
+    (*   ] *)
+    (* in *)
     let conditions =
       [
-        (* (let crossover = *)
-        (*    prev.fast_stochastic_oscillator_k *)
-        (*    >=. prev.fast_stochastic_oscillator_d *)
-        (*    && i.fast_stochastic_oscillator_k <=. i.fast_stochastic_oscillator_d *)
-        (*  in *)
-        (*  let overbought = i.fast_stochastic_oscillator_d >=. 80.0 in *)
-        (*  match overbought && crossover with *)
-        (*  | true -> F.Pass [ "Bearish Crossover" ] *)
-        (*  | false -> F.Fail [ "No Crossover" ]); *)
-        (* (match price <=. Param.stop_loss_multiplier *. buying_order.price with *)
-        (* | true -> F.Pass [ "Stop loss triggered" ] *)
-        (* | false -> F.Fail [ "Stop loss not triggered" ]); *)
-        (* (match price >=. Param.profit_multiplier *. buying_order.price with *)
-        (* | true -> F.Pass [ "Take profit" ] *)
-        (* | false -> F.Fail [ "Not profited enough yet" ]); *)
         (match state.tick >= buying_order.tick + Param.max_holding_period with
-        | true -> F.Pass [ "Holding period exceeded" ]
+        | true -> F.Pass [ "Get out! An hour!!" ]
         | false -> F.Fail [ "Holding period OK" ]);
+        (* (match state.tick >= buying_order.tick + 35 with *)
+        (* | true -> ( *)
+        (*     match *)
+        (*       price <=. Param.stop_loss_multiplier *. buying_order.price *)
+        (*     with *)
+        (*     | true -> F.Pass [ "Price when down after a day" ] *)
+        (*     | false -> F.Fail [ "Price OK" ]) *)
+        (* | false -> F.Fail [ "Holding period OK" ]); *)
       ]
     in
     List.fold_left F.or_fold (Fail []) conditions
