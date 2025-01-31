6f2f332f4d66f1bca58d66298913c93198aa0f32
John Hester <hesterj@etableau.com>
2025-01-31 03:07:37 -0600

    wip: crossover experimentation

--- strategies/crossover.ml
+++ strategies/crossover.ml
@@ -15,15 +15,36 @@ let ( let* ) = F.Infix.( let* )
 
 (* We need a module to see what symbols pass our buy filter, and a way to score the passes *)
 module Buy_inp : Template.Buy_trigger.INPUT = struct
+  let confirmed_crossover (state : 'a State.t) symbol =
+    let indicator = Indicators.get_top state.indicators symbol in
+    let* prev = indicator.previous in
+    let* prev_prev = prev.previous in
+    let crossover =
+      prev_prev.fast_stochastic_oscillator_k
+      <=. prev_prev.fast_stochastic_oscillator_d
+      && prev.fast_stochastic_oscillator_k >=. prev.fast_stochastic_oscillator_d
+      && indicator.fast_stochastic_oscillator_k
+         >=. indicator.fast_stochastic_oscillator_d
+    in
+    match crossover with
+    | true -> F.Pass [ "Bullish Crossover" ]
+    | false -> F.Fail [ "No Crossover" ]
+
   let pass (state : 'a State.t) symbol =
     let price = State.price state symbol in
     let i = Indicators.get_top state.indicators symbol in
     let* prev = i.previous in
     let conditions =
       [
-        (match i.relative_strength_index <=. 40.0 with
+        (match i.relative_strength_index >=. 40.0 with
         | true -> F.Pass [ "Small RSI" ]
         | false -> Fail [ "RSI too large to buy" ]);
+        (* confirmed_crossover state symbol; *)
+        (* ( *)
+        (*   match price >=. i.sma_34 with *)
+        (*   | true -> F.Pass [ "Confirm SMA" ] *)
+        (*   | false -> F.Fail [ "Low" ] *)
+        (* ); *)
         (match i.fast_stochastic_oscillator_d <=. 20.0 with
         | true -> F.Pass [ "FSO %D <= 20" ]
         | false -> F.Fail [ "FSO %D is too high" ]);
@@ -35,9 +56,9 @@ module Buy_inp : Template.Buy_trigger.INPUT = struct
          match crossover with
          | true -> F.Pass [ "Bullish Crossover" ]
          | false -> F.Fail [ "No Crossover" ]);
-        (match price <=. i.sma_233 with
-        | true -> F.Pass [ "Below SMA confirm" ]
-        | false -> F.Fail [ "price above SMA" ]);
+        (* (match price <=. i.sma_233 with *)
+        (* | true -> F.Pass [ "Below SMA confirm" ] *)
+        (* | false -> F.Fail [ "price above SMA" ]); *)
       ]
     in
     List.fold_left F.and_fold (Pass []) conditions
@@ -50,7 +71,7 @@ module Buy_inp : Template.Buy_trigger.INPUT = struct
   (* let lower_bb = I.get_indicator state.indicators symbol P.lower_bollinger in *)
   (* lower_bb /. price *)
 
-  let num_positions = 4
+  let num_positions = 1
 end
 
 (* The functor uses the score to choose the symbol with the highest score *)
@@ -83,6 +104,12 @@ module Sell : Template.Sell_trigger.S = struct
     (* in *)
     let conditions =
       [
+        (* (match i.fast_stochastic_oscillator_d >=. 80.0 with *)
+        (* | true -> F.Pass [ "high %D" ] *)
+        (* | false -> F.Fail [ "low %D" ]); *)
+        (* (match price <=. Param.stop_loss_multiplier *. buying_order.price with *)
+        (* | true -> F.Pass [ "Price when down after a day" ] *)
+        (* | false -> F.Fail [ "Price OK" ]); *)
         (match state.tick >= buying_order.tick + Param.max_holding_period with
         | true -> F.Pass [ "Get out! An hour!!" ]
         | false -> F.Fail [ "Holding period OK" ]);
