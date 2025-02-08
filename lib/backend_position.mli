69eacc94742247e36e993a6695aed04a36bb0235
John Hester <hesterj@etableau.com>
2025-02-08 12:26:09 -0600

    feat: backend position is pure

---
+++ lib/backend_position.mli
@@ -0,0 +1,9 @@
+type t
+
+val execute_order : t -> Order.t -> (t, Error.t) Result.t
+val liquidate : t -> Bars.Latest.t -> (t, Error.t) Result.t
+val get_cash : t -> float
+val set_cash : t -> float -> t
+val symbols : t -> string list
+val qty : t -> string -> int
+val value : t -> Bars.Latest.t -> float
