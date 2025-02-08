e4828f3fb8067d428ed27045cd75f19704ccb106
John Hester <hesterj@etableau.com>
2025-02-07 23:11:41 -0600

    feat: tearsheet, orderhistory is pure

--- lib/order_history.ml
+++ lib/order_history.ml
@@ -1,12 +1,17 @@
 type t = { inactive : Order.t list; active : Order.t list }
 
-let all h = h.inactive @ h.active |> List.sort Order.cmp_timestamp
+let all h = h.inactive @ h.active
+let sort h = List.sort Order.cmp_timestamp h
+let inactive h = h.inactive
+let active h = h.active
 
 let yojson_of_t (h : t) : Yojson.Safe.t =
   let l = all h in
   `List (List.map Order.yojson_of_t l)
 
-let activate x order = { x with active = order :: x.active }
+let add x order = { x with active = order :: x.active }
+let empty = { inactive = []; active = [] }
+let length h = List.length h.inactive + List.length h.active
 
 let complete history order =
   {
