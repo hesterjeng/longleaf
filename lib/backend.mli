a08d6d4f556188793e8d8670b8a28bc2b0e0a5d2
John Hester <hesterj@etableau.com>
2025-03-14 18:04:44 -0500

    feat: lib uses result more

--- lib/backend.mli
+++ lib/backend.mli
@@ -1,8 +1,11 @@
 module type S = Backend_intf.S
 
-val make_bars : Options.t -> Bars.t * Bars.t option
+val make_bars : Options.t -> (Bars.t * Bars.t option, Error.t) result
 
 val make :
-  Options.t -> Bars.t option -> Bars.t option option -> (module Backend_intf.S)
+  Options.t ->
+  Bars.t option ->
+  Bars.t option option ->
+  ((module Backend_intf.S), Error.t) result
 (** Using only the options and context, create an instantiated backend. This
     backend can then be used to instantiate a strategy for running.*)
