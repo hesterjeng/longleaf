e71ef01bd57a25dd57fc55cc141c72a11640456a
John Hester <hesterj@etableau.com>
2025-03-26 16:41:44 -0500

    feat: Instrumt.yojson_of_t

--- lib/instrument.ml
+++ lib/instrument.ml
@@ -1,3 +1,5 @@
 type t = Security of string | Contract of Contract.t [@@deriving show, eq]
 
 let symbol = function Security x -> x | Contract x -> x.symbol
+
+let yojson_of_t (x : t) = `String (symbol x)
