55ad7492a862b362ea1a3bd857fc6b94d838f2f4
John Hester <hesterj@etableau.com>
2025-03-28 22:20:53 -0500

    feat: instrument is hashed type

--- lib/instrument.ml
+++ lib/instrument.ml
@@ -1,6 +1,8 @@
-type t = Security of string | Contract of Contract.t [@@deriving show, eq]
+type t = Security of string | Contract of Contract.t [@@deriving show]
 
 let symbol = function Security x -> x | Contract x -> x.symbol
+let equal x y = String.equal (symbol x) (symbol y)
+let hash x = String.hash @@ symbol x
 let yojson_of_t (x : t) : Yojson.Safe.t = `String (symbol x)
 let t_of_yojson (_ : Yojson.Safe.t) = invalid_arg "Instrument.t_of_yojson NYI"
 let security x = Security x
