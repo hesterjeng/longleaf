1a07c88ff4a9c831ed1bf246185c20880b217524
John Hester <hesterj@etableau.com>
2025-04-04 23:08:59 -0500

    feat: new infix signal operators

--- lib/signal.ml
+++ lib/signal.ml
@@ -4,5 +4,8 @@ end
 
 module Side = Trading_types.Side
 
+let ( let$ ) x f = match x with None -> Ok None | Some x -> f x
+let ( let& ) x f = match x with true -> f () | false -> Ok None
+
 type t = { instrument : Instrument.t; side : Side.t; reason : string list }
 [@@deriving show { with_path = false }]
