a76b9884deb00da2b9a1eed709e894364ed5453d
John Hester <hesterj@etableau.com>
2025-04-09 20:59:13 -0500

    feat: new signal type

--- lib/signal.ml
+++ lib/signal.ml
@@ -4,8 +4,29 @@ end
 
 module Side = Trading_types.Side
 
-let ( let$ ) x f = match x with None -> Ok None | Some x -> f x
-let ( let&& ) x f = match x with true -> f () | false -> Ok None
+(* let ( let&& ) x f = match x with true -> f () | false -> Ok None *)
 
-type t = { instrument : Instrument.t; side : Side.t; reason : string list }
+type t = {
+  instrument : Instrument.t;
+  side : Side.t;
+  reason : string list;
+  flag : bool;
+}
 [@@deriving show { with_path = false }]
+
+let make i s b = { instrument = i; side = s; reason = []; flag = b }
+
+let ( let&& ) signal x f =
+  match x with
+  | true, _ -> f ()
+  | false, s -> { signal with flag = false; reason = [ s ] }
+
+let ( let|| ) signal x f =
+  match x with
+  | true, s -> { signal with flag = true; reason = [ s ] }
+  | false, _ -> f ()
+
+let ( let$ ) signal x f =
+  match x with
+  | None -> { signal with flag = false; reason = [ "missing option" ] }
+  | Some x -> f x
