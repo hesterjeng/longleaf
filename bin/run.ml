ad9631e8d2f8ea0651e266a077637294d7f38e38
John Hester <hesterj@etableau.com>
2025-05-25 17:55:09 -0500

    feate: Run.mk_context, creates context in its own functin

--- bin/run.ml
+++ bin/run.ml
@@ -29,56 +29,63 @@ let save_received_check ~runtype ~save_received : unit =
       Eio.traceln "Must be live or paper to save received data.";
       exit 1
 
+let mk_context ~runtype ~preload ~stacktrace ~no_gui ~target ~save_received
+    ~eio_env ~strategy_arg ~save_to_file ~nowait_market_open ~print_tick_arg
+    ~precompute_indicators_arg ~switch () : Options.Context.t =
+  let _ = stacktrace in
+  let indicator_type, preload, target =
+    match precompute_indicators_arg with
+    | true ->
+      Eio.traceln "Precomputing indicators...";
+      let preload : Options.Preload.t = Loaded (Options.Preload.load preload) in
+      let target : Options.Preload.t =
+        match target with
+        | Some s -> Loaded (Options.Preload.load (File s))
+        | None -> Options.Preload.None
+        (* Loaded (Options.Preload.load target) *)
+      in
+      (Options.IndicatorType.Precomputed, preload, target)
+    | false ->
+      ( Live,
+        preload,
+        match target with
+        | Some s -> File s
+        | None -> Options.Preload.None )
+  in
+  let longleaf_env = Environment.make () in
+  let mutices = Longleaf_mutex.create () in
+  {
+    strategy = Longleaf_strategies.show strategy_arg;
+    runtype;
+    indicator_type;
+    no_gui;
+    eio_env;
+    longleaf_env;
+    switch;
+    preload;
+    target;
+    save_received;
+    nowait_market_open;
+    mutices;
+    save_to_file;
+    print_tick_arg;
+  }
+
 let top ~runtype ~preload ~stacktrace ~no_gui ~target ~save_received ~eio_env
     ~strategy_arg ~save_to_file ~nowait_market_open ~print_tick_arg
     ~precompute_indicators_arg =
   runtype_target_check ~runtype ~target;
   save_received_check ~runtype ~save_received;
   let _ = stacktrace in
-  let longleaf_env = Environment.make () in
   let domain_manager = Eio.Stdenv.domain_mgr eio_env in
   let mutices = Longleaf_mutex.create () in
   let run_strategy () =
     Eio.Domain_manager.run domain_manager @@ fun () ->
     Eio.Switch.run @@ fun switch ->
-    let context : Options.Context.t =
-      let indicator_type, preload, target =
-        match precompute_indicators_arg with
-        | true ->
-          Eio.traceln "Precomputing indicators...";
-          let preload : Options.Preload.t =
-            Loaded (Options.Preload.load preload)
-          in
-          let target : Options.Preload.t =
-            match target with
-            | Some s -> Loaded (Options.Preload.load (File s))
-            | None -> Options.Preload.None
-            (* Loaded (Options.Preload.load target) *)
-          in
-          (Options.IndicatorType.Precomputed, preload, target)
-        | false ->
-          ( Live,
-            preload,
-            match target with
-            | Some s -> File s
-            | None -> Options.Preload.None )
-      in
-      {
-        strategy = Longleaf_strategies.show strategy_arg;
-        runtype;
-        indicator_type;
-        no_gui;
-        eio_env;
-        longleaf_env;
-        switch;
-        preload;
-        target;
-        save_received;
-        nowait_market_open;
-        mutices;
-        save_to_file;
-        print_tick_arg;
-      }
+    let context =
+      mk_context ~runtype ~preload ~stacktrace ~no_gui ~target ~save_received
+        ~eio_env ~strategy_arg ~save_to_file ~nowait_market_open ~print_tick_arg
+        ~precompute_indicators_arg ~switch ()
     in
     Eio.traceln "@[Context: %a@]@." Options.Context.pp context;
     let res = Longleaf_strategies.run context strategy_arg in
