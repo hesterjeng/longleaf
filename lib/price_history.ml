2c97b4f33e4b1f8465c296cb92f5e52d2e5e2ac2
John Hester <hesterj@etableau.com>
2025-06-09 04:50:40 -0500

    feat: price_history.of_items, item_of_column

--- lib/price_history.ml
+++ lib/price_history.ml
@@ -1,4 +1,11 @@
-open Bigarray
+module Array1 = Bigarray.Array1
+module Array2 = Bigarray.Array2
+
+type float64_elt = Bigarray.float64_elt
+type c_layout = Bigarray.c_layout
+
+let c_layout = Bigarray.c_layout
+let float64 = Bigarray.float64
 
 type t = {
   data : (float, float64_elt, c_layout) Array2.t;
@@ -6,6 +13,7 @@ type t = {
   size : int;
   indicators_computed : bool;
 }
+(* [@@deriving show] *)
 
 type slice = (float, float64_elt, c_layout) Array1.t
 
@@ -42,8 +50,21 @@ module Row = struct
     | RSI -> 11
 end
 
+(* let pp_array : (float, float64_elt, c_layout) Array2.t Format.printer = *)
+(*   invalid_arg "nyi" *)
+
 let length x = Array2.dim2 x.data
 let current x = x.current
+
+(* let item_of_column x i : (Item.t, Error.t) result = *)
+(*   match *)
+
+(* let items (x : t) = *)
+
+(* let collect_column x i = *)
+(*   match i < x.size && i >= 0 with *)
+(*   | false -> Error.fatal "Price_history.collect_column: illegal column access" *)
+(*   | true -> *)
 
 (* Row 0 : i (index)*)
 (* Row 1 : time (float) *)
@@ -61,12 +82,44 @@ let current x = x.current
 let set (res : t) (x : Row.t) i value =
   Array2.set res.data (Row.to_int x) i @@ value
 
-let get (res : t) (x : Row.t) i = Array2.get res.data (Row.to_int x) i
-let get_top (res : t) (x : Row.t) = get res x @@ res.current
+let get (res : t) (x : Row.t) i =
+  let res = Array2.get res.data (Row.to_int x) i in
+  assert (not @@ Float.is_nan res);
+  res
+
+let get_top (res : t) (x : Row.t) =
+  let res = get res x @@ res.current in
+  assert (not @@ Float.is_nan res);
+  res
+
+let item_of_column x i =
+  let open Item in
+  {
+    timestamp =
+      Ptime.of_float_s @@ get x Time i
+      |> Option.get_exn_or
+           "Expected to get a valid time in Price_history.item_of_column";
+    open_ = get x Open i;
+    high = get x High i;
+    low = get x Low i;
+    close = get x Close i;
+    last = get x Last i;
+    volume = get x Volume i |> Float.to_int;
+  }
+
+let to_items (x : t) =
+  let rec aux i acc =
+    match i >= x.size with
+    | true -> acc
+    | false ->
+      let item = item_of_column x i in
+      aux (i + i) @@ (item :: acc)
+  in
+  aux 0 []
 
 let make size : t =
   {
-    data = Array2.create float64 c_layout Row.count size;
+    data = Array2.init float64 c_layout Row.count size (fun _ _ -> Float.nan);
     current = 0;
     size;
     indicators_computed = false;
@@ -77,8 +130,7 @@ let copy (x : t) =
   Array2.blit x.data r.data;
   { r with current = x.current; indicators_computed = x.indicators_computed }
 
-let add_item (x : t) (item : Item.t) =
-  let i = x.current in
+let set_item (x : t) (i : int) (item : Item.t) =
   try
     set x Index i (Float.of_int i);
     set x Time i @@ Ptime.to_float_s @@ Item.timestamp item;
@@ -91,6 +143,24 @@ let add_item (x : t) (item : Item.t) =
     Ok ()
   with
   | _ -> Error.fatal "Illegal index accessin Price_history.V2.add_item"
+
+let add_item (x : t) (item : Item.t) =
+  let ( let* ) = Result.( let* ) in
+  let* () = set_item x x.current item in
+  Result.return @@ { x with current = x.current + 1 }
+
+let of_items size (l : Item.t list) =
+  let ( let* ) = Result.( let* ) in
+  let matrix = make size in
+  let sorted = List.sort Item.compare l in
+  let* res =
+    List.fold_left
+      (fun acc item ->
+        let* acc = acc in
+        add_item acc item)
+      (Ok matrix) sorted
+  in
+  Result.return res
 
 let t_of_yojson (json : Yojson.Safe.t) : (t, Error.t) result =
   let ( let* ) = Result.( let* ) in
@@ -106,10 +176,12 @@ let t_of_yojson (json : Yojson.Safe.t) : (t, Error.t) result =
         let current = Item.t_of_yojson current_j in
         assert (i < size);
         assert (get res SMA i =. 0.0);
-        let* () = add_item res current in
-        aux (i + 1) xs @@ Result.return { acc with current = acc.current + 1 }
+        let* acc = add_item acc current in
+        aux (i + 1) xs @@ Result.return acc
     in
     let* res = aux 0 items (Ok res) in
     Result.return res
   | _ ->
     Error.json "Expected a list of datapoints in Price_history.V2.t_of_yojson"
+
+let combine (l : t list) = Array2.slice_left
