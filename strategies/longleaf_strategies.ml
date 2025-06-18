8c8090374578496d1f87bb32ad702718134b9705
John Hester <hesterj@etableau.com>
2025-06-18 07:20:47 -0500

    feat: move run_generic to one location

--- strategies/longleaf_strategies.ml
+++ strategies/longleaf_strategies.ml
@@ -1,56 +1,6 @@
 module Context = Options.Context
 module Collections = Ticker_collections
 
-(** Function for creating the options given a context, it has some sensible
-    defaults. If you want to use other options, you may need to create your own
-    Options.t value. *)
-let run_options (context : Context.t) : Options.t =
-  let symbols =
-    match context.runtype with
-    | RandomTickerBacktest
-    | MultiRandomTickerBacktest ->
-      let arr = Array.of_list Collections.sp100 in
-      let eighty_percent =
-        (Array.length arr |> Float.of_int) *. 0.8 |> Int.of_float
-      in
-      Owl_stats.choose arr eighty_percent |> Array.to_list
-    | _ -> Collections.sp100
-  in
-  {
-    symbols;
-    tick = 600.0;
-    overnight = true;
-    resume_after_liquidate = true;
-    indicators_config : Indicator_config.t =
-      { fft = false; compare_preloaded = context.compare_preloaded };
-    dropout = false;
-    randomized_backtest_length = 1000;
-    context;
-  }
-
-(** Helper function to reduce code duplication. *)
-let run_generic ?(run_options = run_options) ?bars ?target
-    (module Strat : Strategy.BUILDER) context =
-  let options = run_options context in
-  let bars =
-    match bars with
-    | None ->
-      invalid_arg
-        "Longleaf_strategies.run_generic: Cannot run_generic with bars None"
-    | Some x -> x
-  in
-  (* let () = check_bars options in *)
-  let module Backend =
-    (val Backend.make options bars target |> function
-         | Ok x -> x
-         | Error _ -> invalid_arg "Error while creating backend")
-  in
-  let module S = Strat (Backend) in
-  Eio.traceln "Applied strategy functor to backend, running.";
-  let res = S.run () in
-  Backend.shutdown ();
-  res
-
 (** Type of strategies that have been defined. To add a new strategy, you must
     first add a corresponding variant to this type. Afterwards, you must add a
     handler for your strategy in the strats value below. *)
@@ -130,7 +80,7 @@ type multitest = { mean : float; min : float; max : float; std : float }
 let run_astar (context : Context.t) ~(buy : Astar_search.EnumeratedSignal.t)
     ~(sell : Astar_search.EnumeratedSignal.t) =
   let x = Astar_search.EnumeratedSignal.to_strategy buy sell in
-  run_generic x context
+  Run.top x context
 
 (** Top level function for running strategies based on a context.*)
 let run (context : Context.t) strategy =
