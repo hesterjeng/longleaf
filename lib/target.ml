cf759e6c390e1670c44d5f0962a620d014e54fcc
John Hester <hesterj@etableau.com>
2025-06-23 13:51:01 -0500

    feat: add target.ml

---
+++ lib/target.ml
@@ -0,0 +1,42 @@
+(* Start with empty bars, load bars from a file, or download data *)
+type t =
+  | Download
+  | Loaded of Bars.t (* None | File of string | Download | Loaded of Bars.t *)
+[@@deriving show, variants]
+
+let of_string_res x =
+  match x with
+  (* | "None" *)
+  (* | "none" -> *)
+  (*   Ok None *)
+  | "Download"
+  | "download" ->
+    Ok Download
+  | s when Sys.file_exists s ->
+    let res = Bars.of_file s in
+    Result.return @@ Loaded res
+  | _ ->
+    Error (`Msg "Expected a valid preload selection, or file doesn't exist")
+
+(* let pp_opt : t option Format.printer = *)
+(*   fun fmt x -> *)
+(*   Format.fprintf fmt "%a" (Option.pp pp) x *)
+
+(* let of_string_opt_res x = *)
+(*   match x with *)
+(*   | Some s -> of_string_res s *)
+(*   | None -> Error (`Msg "The target argument is required") *)
+
+let conv = Cmdliner.Arg.conv (of_string_res, pp)
+
+(* let load = function *)
+(*   | None -> invalid_arg "Cannot load missing preload in Options.Preload.load" *)
+(*   | File s -> *)
+(*     let res = Bars.of_file s in *)
+(*     Loaded res *)
+(*   | Download -> invalid_arg "Cannot load download in Options.Preload.load" *)
+(*   | Loaded b -> Loaded b *)
+
+let bars = function
+  | Loaded b -> b
+  | _ -> invalid_arg "Preload.bars: bars not loaded"
