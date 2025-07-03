25b6b1ed768ec381a85eae7e7ed69750ffd39489
John Hester <hesterj@etableau.com>
2025-07-03 13:09:13 -0500

    wip: flags to control indicator computation

--- lib/indicator_config.ml
+++ lib/indicator_config.ml
@@ -1,3 +1,4 @@
-type t = { fft : bool; compare_preloaded : bool } [@@deriving show]
+type t = { fft : bool; compare_preloaded : bool; compute_live : bool }
+[@@deriving show]
 
-let default = { fft = false; compare_preloaded = false }
+let default = { fft = false; compare_preloaded = false; compute_live = false }
--- lib/indicators.ml
+++ lib/indicators.ml
@@ -9,24 +9,27 @@ let get_ohclv (x : Data.t) : Tacaml.Ohlcv.t =
     volume = Data.get_row x Volume;
   }
 
-let compute_all (bars : Bars.t) =
-  let ( let* ) = Result.( let* ) in
-  let* length = Bars.length bars in
-  Bars.fold bars (Ok ()) @@ fun _ data acc ->
-  let* _ = acc in
-  let close = Data.get_row data Close in
-  let ohclv = get_ohclv data in
-  let rsi = Data.get_row data RSI |> Data.Row.slice 14 (length - 14) in
-  let slow_k = Data.get_row data FSO_K |> Data.Row.slice 17 (length - 17) in
-  let slow_d = Data.get_row data FSO_D |> Data.Row.slice 17 (length - 17) in
-  let* _, _ = TA.ta_rsi (0, length - 1) close 14 rsi in
-  let* _, _ = TA.ta_stoch (0, length - 1) ohclv 14 3 3 slow_k slow_d in
-  (* Eio.traceln "%a" (Data.pp_row RSI) data; *)
-  (* Eio.traceln "%d" length; *)
-  (* Eio.traceln "%d, %d" outBegIdx outNBElement; *)
-  (* Eio.traceln "%a" (Data.pp_row FSO_K) data; *)
-  (* Error.fatal "NYI" *)
-  Result.return ()
+let compute_all (config : Indicator_config.t) (bars : Bars.t) =
+  match config.compute_live with
+  | false ->
+    let ( let* ) = Result.( let* ) in
+    let* length = Bars.length bars in
+    Bars.fold bars (Ok ()) @@ fun _ data acc ->
+    let* _ = acc in
+    let close = Data.get_row data Close in
+    let ohclv = get_ohclv data in
+    let rsi = Data.get_row data RSI |> Data.Row.slice 14 (length - 14) in
+    let slow_k = Data.get_row data FSO_K |> Data.Row.slice 17 (length - 17) in
+    let slow_d = Data.get_row data FSO_D |> Data.Row.slice 17 (length - 17) in
+    let* _, _ = TA.ta_rsi (0, length - 1) close 14 rsi in
+    let* _, _ = TA.ta_stoch (0, length - 1) ohclv 14 3 3 slow_k slow_d in
+    (* Eio.traceln "%a" (Data.pp_row RSI) data; *)
+    (* Eio.traceln "%d" length; *)
+    (* Eio.traceln "%d, %d" outBegIdx outNBElement; *)
+    (* Eio.traceln "%a" (Data.pp_row FSO_K) data; *)
+    (* Error.fatal "NYI" *)
+    Result.return ()
+  | true -> Result.return ()
 
 let initialize () =
   match TA.ta_initialize () with
