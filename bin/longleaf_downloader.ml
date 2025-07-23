ab6dd4812e2fbea51e1f709bc299c2a24f4bb77e
John Hester <hesterj@etableau.com>
2025-07-23 14:26:56 -0500

    feat: add downloader.ml

--- bin/longleaf_downloader.ml
+++ bin/longleaf_downloader.ml
@@ -1,19 +1,8 @@
 open Longleaf_core
 module Collections = Longleaf_strategies.Collections
-module Bars = Longleaf_bars
 module Market_data_api = Longleaf_apis.Market_data_api
-module Tiingo_api = Longleaf_apis.Tiingo_api
-
-module Downloader_ty = struct
-  type t = Alpaca | Tiingo [@@deriving show]
-
-  let of_string = function
-    | "alpaca" -> Ok Alpaca
-    | "tiingo" -> Ok Tiingo
-    | _ -> invalid_arg "Unknown downloader service"
-
-  let conv = Cmdliner.Arg.conv (of_string, pp)
-end
+module Downloader = Longleaf_apis.Downloader
+module Ty = Downloader.Ty
 
 module Args = struct
   (* Define the CLI arguments *)
@@ -27,7 +16,7 @@ module Args = struct
 
   let downloader_arg =
     let doc = "Choose downloader client.  Currently Tiingo or Alpaca." in
-    Cmdliner.Arg.(value & pos 0 (some Downloader_ty.conv) None & info [] ~doc)
+    Cmdliner.Arg.(value & pos 0 (some Ty.conv) None & info [] ~doc)
 
   let begin_arg =
     let doc = "Begin date as YYYY-MM-DD." in
@@ -57,60 +46,6 @@ module Args = struct
       value & pos 1 (some string) None & info [] ~docv:"output file" ~doc)
 end
 
-module Downloader = struct
-  let data_client switch eio_env =
-    let res =
-      Piaf.Client.create ~sw:switch eio_env Longleaf_util.apca_api_data_url
-    in
-    match res with
-    | Ok x -> x
-    | Error _ -> invalid_arg "Unable to create data client"
-
-  let top eio_env request prefix output_file
-      (downloader_arg : Downloader_ty.t option) afterhours =
-    Eio.Switch.run @@ fun switch ->
-    let ( let* ) = Result.( let* ) in
-    (* Util.yojson_safe true @@ fun () -> *)
-    let longleaf_env = Longleaf_core.Environment.make () in
-    let data_client = data_client switch eio_env in
-    let module Conn : Longleaf_apis.Client.CLIENT = struct
-      let client = data_client
-      let longleaf_env = longleaf_env
-    end in
-    let module MDA = Market_data_api.Make (Conn) in
-    Eio.traceln "Making request %a..." Market_data_api.Request.pp request;
-    let* bars =
-      match downloader_arg with
-      | Some Alpaca -> (
-        MDA.Stock.historical_bars request |> function
-        | Ok x -> x
-        | Error e ->
-          Eio.traceln "%a" Error.pp e;
-          invalid_arg "Error downloading historical bars")
-      | Some Tiingo ->
-        let module Param = struct
-          let longleaf_env = longleaf_env
-          let client = Tiingo_api.tiingo_client eio_env switch
-        end in
-        let module Tiingo = Tiingo_api.Make (Param) in
-        let res = Tiingo.Data.top ~afterhours request in
-        Piaf.Client.shutdown Param.client;
-        res
-      | None ->
-        invalid_arg "Need to specify downloader type for data_downloader."
-    in
-    (* Bars.Infill.top bars; *)
-    Eio.traceln "%a" Bars.pp_stats bars;
-    (* Bars.sort Longleaf_lib.Item.compare bars; *)
-    let* () =
-      match output_file with
-      | Some filename -> Bars.print_to_file_direct bars filename
-      | None -> Bars.print_to_file bars prefix
-    in
-    Piaf.Client.shutdown data_client;
-    Result.return ()
-end
-
 module Cmd = struct
   let run today begin_arg end_arg timeframe_arg interval_arg output_file_arg
       downloader_arg afterhours_arg =
@@ -138,7 +73,7 @@ module Cmd = struct
     in
     let _ =
       Eio_main.run @@ fun eio_env ->
-      Downloader.top eio_env request prefix output_file_arg downloader_arg
+      Downloader.download eio_env request prefix output_file_arg downloader_arg
         afterhours_arg
     in
     ()
---
+++ lib/apis/downloader.ml
@@ -0,0 +1,63 @@
+module Bars = Longleaf_bars
+
+module Ty = struct
+  type t = Alpaca | Tiingo [@@deriving show]
+
+  let of_string = function
+    | "alpaca" -> Ok Alpaca
+    | "tiingo" -> Ok Tiingo
+    | _ -> invalid_arg "Unknown downloader service"
+
+  let conv = Cmdliner.Arg.conv (of_string, pp)
+end
+
+let data_client switch eio_env =
+  let res =
+    Piaf.Client.create ~sw:switch eio_env Longleaf_util.apca_api_data_url
+  in
+  match res with
+  | Ok x -> x
+  | Error _ -> invalid_arg "Unable to create data client"
+
+let download eio_env request prefix output_file (downloader_arg : Ty.t option)
+    afterhours =
+  Eio.Switch.run @@ fun switch ->
+  let ( let* ) = Result.( let* ) in
+  (* Util.yojson_safe true @@ fun () -> *)
+  let longleaf_env = Longleaf_core.Environment.make () in
+  let data_client = data_client switch eio_env in
+  let module Conn : Client.CLIENT = struct
+    let client = data_client
+    let longleaf_env = longleaf_env
+  end in
+  let module MDA = Market_data_api.Make (Conn) in
+  Eio.traceln "Making request %a..." Market_data_api.Request.pp request;
+  let* bars =
+    match downloader_arg with
+    | Some Alpaca -> (
+      MDA.Stock.historical_bars request |> function
+      | Ok x -> x
+      | Error e ->
+        Eio.traceln "%a" Error.pp e;
+        invalid_arg "Error downloading historical bars")
+    | Some Tiingo ->
+      let module Param = struct
+        let longleaf_env = longleaf_env
+        let client = Tiingo_api.tiingo_client eio_env switch
+      end in
+      let module Tiingo = Tiingo_api.Make (Param) in
+      let res = Tiingo.Data.top ~afterhours request in
+      Piaf.Client.shutdown Param.client;
+      res
+    | None -> invalid_arg "Need to specify downloader type for data_downloader."
+  in
+  (* Bars.Infill.top bars; *)
+  Eio.traceln "%a" Bars.pp_stats bars;
+  (* Bars.sort Longleaf_lib.Item.compare bars; *)
+  let* () =
+    match output_file with
+    | Some filename -> Bars.print_to_file_direct bars filename
+    | None -> Bars.print_to_file bars prefix
+  in
+  Piaf.Client.shutdown data_client;
+  Result.return ()
