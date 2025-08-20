fe678f5a1e1814134fd427d8388cc8bfb5ad2d1e
John Hester <hesterj@etableau.com>
2025-08-20 11:13:12 -0500

    feat: settings endpoint

--- bin/longleaf_server.ml
+++ bin/longleaf_server.ml
@@ -8,29 +8,38 @@ open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
 module Settings = struct
   type t = {
-    mutable cli_vars : (Core.Options.CLI.t, Error.t) result;
-    mutable target : (Core.Target.t, Error.t) result;
+    mutable cli_vars : Core.Options.CLI.t option;
+    mutable target : Core.Target.t option;
   }
   [@@deriving show, yojson]
 
-  let init =
-    {
-      cli_vars = Error.fatal "longleaf_server: CLI vars not initialized";
-      target = Error.fatal "longleaf server: target not initialized";
-    }
+  let settings = { cli_vars = None; target = None }
+
+  let get () =
+    (Option.is_some settings.cli_vars && Option.is_some settings.target)
+    |> function
+    | true -> Result.return settings
+    | false -> Error.fatal "CLI vars or target not initialized"
 end
 
 let get =
   [
     Dream.get "/static/**" @@ Dream.static "static";
     (Dream.get "/status" @@ fun _ -> Dream.html "Place status here...");
-    ( Dream.get "/get_data" @@ fun _ ->
+    ( Dream.get "/settings" @@ fun _ ->
+      match Settings.get () with
+      | Ok settings ->
+        Settings.yojson_of_t settings |> Yojson.Safe.to_string |> Dream.json
+      | Error e ->
+        let s = Error.show e in
+        Dream.respond ~status:`Internal_Server_Error s );
+    ( Dream.get "/data" @@ fun _ ->
       Sys.readdir "data" |> Array.to_list
       |> List.filter (fun x -> String.equal (Filename.extension x) ".json")
       |> List.map (fun x -> `String x)
       |> fun x -> `List x |> Yojson.Safe.to_string |> Dream.json );
-    (Dream.get "/get_options" @@ fun _ -> Dream.html "Show configured options");
-    ( Dream.get "/get_strategies" @@ fun _ ->
+    (Dream.get "/options" @@ fun _ -> Dream.html "Show configured options");
+    ( Dream.get "/strategies" @@ fun _ ->
       List.map (fun x -> `String x) Longleaf_strategies.all_strategy_names
       |> fun x -> `List x |> Yojson.Safe.to_string |> Dream.json );
     Dream.get "/" @@ Dream.from_filesystem "static" "index.html";
