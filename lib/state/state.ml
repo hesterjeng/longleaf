6586b7b1515abb7d06f2e2f3b2a956d81f2e4fe6
John Hester <hesterj@etableau.com>
2025-09-22 14:03:39 -0500

    wip: benchmark data used correctly

--- lib/state/state.ml
+++ lib/state/state.ml
@@ -150,28 +150,57 @@ let orders_placed x = x.orders_placed
 
 module Conv = struct
   let to_tearsheet_json x =
-    let vh = List.rev (value_history x) in (* Reverse to get chronological order *)
+    let vh = List.rev (value_history x) in
+    (* Reverse to get chronological order *)
     match vh with
-    | [] -> `Assoc [("returns", `List []); ("dates", `List [])]
+    | [] -> `Assoc [ ("returns", `List []); ("dates", `List []) ]
     | _ ->
       let values = List.map snd vh in
       let dates = List.map (fun (t, _) -> Time.to_string t) vh in
-      
+
       (* Calculate returns from consecutive portfolio values *)
       let rec calc_returns acc = function
-        | [] | [_] -> List.rev acc (* Need at least 2 values *)
+        | []
+        | [ _ ] ->
+          List.rev acc (* Need at least 2 values *)
         | prev :: (curr :: _ as rest) ->
           let return_pct = (curr -. prev) /. prev in
           calc_returns (return_pct :: acc) rest
       in
       let returns = calc_returns [] values in
-      
+      let benchmark_returns : Yojson.Safe.t =
+        (let ( let* ) = Result.( let* ) in
+         let* spy = Bars.get (bars x) (Security "SPY") in
+         let* prices = Bars.Data.get_row spy Last in
+         let rec loop i acc =
+           if i < 0 then acc
+           else loop (i - 1) (Bigarray.Array1.get prices i :: acc)
+         in
+         let l =
+           loop (Bigarray.Array1.dim prices - 1) []
+           |> List.rev |> calc_returns []
+           |> List.map (fun x -> `Float x)
+           |> fun l -> `List l
+         in
+         Result.return l)
+        |> function
+        | Ok x -> x
+        | Error _ -> `List []
+      in
+
       (* Format for JSON *)
       let returns_json = List.map (fun r -> `Float r) returns in
-      let dates_json = List.map (fun d -> `String d) (List.tl dates) in (* Skip first date since we have n-1 returns *)
-      
-      `Assoc [
-        ("returns", `List returns_json);
-        ("dates", `List dates_json)
-      ]
+      let dates_json = List.map (fun d -> `String d) (List.tl dates) in
+      (* Skip first date since we have n-1 returns *)
+
+      `Assoc
+        [
+          ("returns", `List returns_json);
+          ("dates", `List dates_json);
+          ("periods", `Int 9828);
+          ("title", `String "tearsheet");
+          ("benchmark_returns", benchmark_returns);
+          ("benchmark_dates", `List dates_json);
+          ("benchmark_name", `String "SPY");
+        ]
 end
