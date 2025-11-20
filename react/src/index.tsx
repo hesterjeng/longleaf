7ef70e92687cbb7c22f8837012bbf2bdd252d3e3
John Hester <hesterj@etableau.com>
2025-11-20 02:24:50 -0600

    fix: axios base url

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import 'antd/dist/reset.css';
 import './styles.css';
 import App from './App';
+import { API_CONFIG } from './utils/constants';
+
+// Configure axios baseURL from environment or default
+axios.defaults.baseURL = API_CONFIG.baseURL;
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
